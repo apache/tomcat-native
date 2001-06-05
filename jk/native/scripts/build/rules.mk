68d022fce18adab0f76a92865a0920b23989b53d
Jean-Frederic Clere <jfclere@apache.org>
2001-06-05 10:27:58 +0000

    Add support for staticly linked mod_jk for Apache-1.3.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@295388 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jk/native/scripts/build/rules.mk
@@ -0,0 +1,30 @@
+# That an extract of what is in APR.
+#
+
+# may be libtool as to be the one of APR (that just not to forget it).
+LIBTOOL      = libtool
+
+# Compile commands
+
+COMPILE      = $(CC) $(CFLAGS)
+LT_COMPILE   = $(LIBTOOL) --mode=compile $(COMPILE) -c $< && touch $@
+
+# Implicit rules for creating outputs from input files
+
+.SUFFIXES:
+.SUFFIXES: .c .lo .o .slo .s
+
+.c.o:
+	$(COMPILE) -c $<
+
+.s.o:
+	$(COMPILE) -c $<
+
+.c.lo:
+	$(LT_COMPILE)
+
+.s.lo:
+	$(LT_COMPILE)
+
+.c.slo:
+	$(SH_COMPILE)
