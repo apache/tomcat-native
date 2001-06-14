011f289a0d464aad8192dbe3496a3e7d85ae4ba6
Remy Maucherat <remm@apache.org>
2001-06-14 01:07:51 +0000

    Initial revision
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@295445 13f79535-47bb-0310-9956-ffa450edef68

---
+++ coyote/src/java/org/apache/coyote/Request.java
@@ -0,0 +1,406 @@
+/*
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Tomcat", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * [Additional notices, if required by prior licensing conditions]
+ *
+ */
+
+
+package org.apache.coyote;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import org.apache.tomcat.util.buf.MessageBytes;
+import org.apache.tomcat.util.buf.UDecoder;
+
+import org.apache.tomcat.util.http.MimeHeaders;
+import org.apache.tomcat.util.http.Parameters;
+import org.apache.tomcat.util.http.ContentType;
+import org.apache.tomcat.util.http.Cookies;
+
+/**
+ * This is a low-level, efficient representation of a server request. Most 
+ * fields are GC-free, expensive operations are delayed until the  user code 
+ * needs the information.
+ *
+ * Processing is delegated to modules, using a hook mechanism.
+ * 
+ * This class is not intended for user code - it is used internally by tomcat
+ * for processing the request in the most efficient way. Users ( servlets ) can
+ * access the information using a facade, which provides the high-level view
+ * of the request.
+ *
+ * For lazy evaluation, the request uses the getInfo() hook. The following ids
+ * are defined:
+ * <ul>
+ *  <li>req.encoding - returns the request encoding
+ *  <li>req.attribute - returns a module-specific attribute ( like SSL keys, etc ).
+ * </ul>
+ *
+ * Tomcat defines a number of attributes:
+ * <ul>
+ *   <li>"org.apache.tomcat.request" - allows access to the low-level
+ *       request object in trusted applications 
+ * </ul>
+ *
+ * @author James Duncan Davidson [duncan@eng.sun.com]
+ * @author James Todd [gonzo@eng.sun.com]
+ * @author Jason Hunter [jch@eng.sun.com]
+ * @author Harish Prabandham
+ * @author Alex Cruikshank [alex@epitonic.com]
+ * @author Hans Bergsten [hans@gefionsoftware.com]
+ * @author Costin Manolache
+ * @author Remy Maucherat
+ */
+public final class Request {
+
+
+    // ----------------------------------------------------------- Constructors
+
+
+    public Request() {
+ 	headers = new MimeHeaders();
+	scookies = new Cookies(headers);
+	initRequest();
+    }
+
+
+    // ----------------------------------------------------- Instance Variables
+
+
+    protected int serverPort;
+    protected String localHost;
+
+    protected MessageBytes schemeMB = new MessageBytes();
+
+    protected MessageBytes methodMB = new MessageBytes();
+    protected MessageBytes unparsedURIMB = new MessageBytes();
+    protected MessageBytes uriMB = new MessageBytes();
+    protected MessageBytes queryMB = new MessageBytes();
+    protected MessageBytes protoMB = new MessageBytes();
+
+    // remote address/host
+    protected MessageBytes remoteAddrMB = new MessageBytes();
+    protected MessageBytes remoteHostMB = new MessageBytes();
+    
+    protected MimeHeaders headers;
+
+    protected int contentLength = -1;
+    // how much body we still have to read.
+    protected int available = -1; 
+
+    protected MessageBytes contentTypeMB = null;
+    protected String charEncoding = null;
+    protected MessageBytes serverNameMB = new MessageBytes();
+
+    protected Cookies scookies;
+
+
+    /**
+     * Notes.
+     */
+    protected Note notes[] = new Note[Constants.MAX_NOTES];
+
+
+    /**
+     * Associated input buffer.
+     */
+    protected InputBuffer inputBuffer = null;
+
+
+    /**
+     * URL decoder.
+     */
+    UDecoder urlDecoder = new UDecoder();
+
+
+    // ------------------------------------------------------------- Properties
+
+
+    public MimeHeaders getMimeHeaders() {
+        return headers;
+    }
+
+
+    public UDecoder getURLDecoder() {
+        return urlDecoder;
+    }
+
+
+    // -------------------- Request data --------------------
+
+
+    public MessageBytes scheme() {
+        return schemeMB;
+    }
+    
+    public MessageBytes method() {
+        return methodMB;
+    }
+    
+    public MessageBytes unparsedURI() {
+        return unparsedURIMB;
+    }
+
+    public MessageBytes requestURI() {
+        return uriMB;
+    }
+
+    public MessageBytes query() {
+        return queryMB;
+    }
+
+    public MessageBytes queryString() {
+        return queryMB;
+    }
+
+    public MessageBytes protocol() {
+        return protoMB;
+    }
+    
+    /** 
+     * Return the buffer holding the server name, if
+     * any. Use isNull() to check if there is no value
+     * set.
+     * This is the "virtual host", derived from the
+     * Host: header.
+     */
+    public MessageBytes serverName() {
+	return serverNameMB;
+    }
+    
+    public int getServerPort() {
+        return serverPort;
+    }
+
+    public void setServerPort(int serverPort ) {
+	this.serverPort=serverPort;
+    }
+    public MessageBytes remoteAddr() {
+	return remoteAddrMB;
+    }
+
+    public MessageBytes remoteHost() {
+	return remoteHostMB;
+    }
+
+    public String getLocalHost() {
+	return localHost;
+    }
+
+    public void setLocalHost(String host) {
+	this.localHost = host;
+    }
+
+
+    // -------------------- encoding/type --------------------
+
+
+    public String getCharacterEncoding() {
+        if(charEncoding != null) return charEncoding;
+
+        charEncoding = ContentType.getCharsetFromContentType(getContentType());
+	return charEncoding;
+    }
+
+
+    public void setCharEncoding(String enc) {
+	this.charEncoding = enc;
+    }
+
+
+    public void setContentLength(int len) {
+	this.contentLength = len;
+	available = len;
+    }
+
+
+    public int getContentLength() {
+        if( contentLength > -1 ) return contentLength;
+
+	MessageBytes clB = headers.getValue("content-length");
+        contentLength = (clB == null || clB.isNull()) ? -1 : clB.getInt();
+	available = contentLength;
+
+	return contentLength;
+    }
+
+
+    public String getContentType() {
+        contentType();
+        if ((contentTypeMB == null) || contentTypeMB.isNull()) 
+            return null;
+        return contentTypeMB.toString();
+    }
+
+
+    public void setContentType(String type) {
+        contentTypeMB.setString(type);
+    }
+
+
+    public MessageBytes contentType() {
+        if (contentTypeMB == null)
+            contentTypeMB = headers.getValue("content-type");
+        return contentTypeMB;
+    }
+
+
+    public void setContentType(MessageBytes mb) {
+        contentTypeMB=mb;
+    }
+
+
+    // -------------------- Cookies --------------------
+
+
+    public Cookies getCookies() {
+	return scookies;
+    }
+
+
+    // -------------------- Input Buffer --------------------
+
+
+    public InputBuffer getInputBuffer() {
+        return inputBuffer;
+    }
+
+
+    public void setInputBuffer(InputBuffer inputBuffer) {
+        this.inputBuffer = inputBuffer;
+    }
+
+
+    /**
+     * Read data from the input buffer and put it into a byte array.
+     */
+    public int doRead(byte b[], int off, int len) 
+        throws IOException {
+        int n = inputBuffer.doRead(b, off, len);
+        if (n > 0)
+            available -= n;
+        return n;
+    }
+
+
+    // -------------------- debug --------------------
+
+
+    public String toString() {
+	return "R( " + requestURI().toString() + ")";
+    }
+
+
+    // -------------------- Per-Request "notes" --------------------
+
+
+    public final void setNote(int pos, Note value) {
+	notes[pos] = value;
+    }
+
+
+    public final Note getNote(int pos) {
+	return notes[pos];
+    }
+
+
+    // -------------------- Recycling -------------------- 
+
+
+    public void recycle() {
+	initRequest();
+    }
+
+
+    public void initRequest() {
+	//params.recycle();
+	contentLength = -1;
+        contentTypeMB = null;
+        charEncoding = null;
+        //didReadFormData = false;
+        headers.clear(); // XXX use recycle pattern
+        serverNameMB.recycle();
+        serverPort=-1;
+	
+	scookies.recycle();
+
+        for (int i=0; i < Constants.MAX_NOTES; i++) {
+            if (notes[i] != null)
+                notes[i].recycle();
+        }
+
+        unparsedURIMB.recycle();
+        uriMB.recycle();
+	queryMB.recycle();
+	methodMB.recycle();
+	protoMB.recycle();
+	remoteAddrMB.recycle();
+	remoteHostMB.recycle();
+
+	// XXX Do we need such defaults ?
+        schemeMB.setString("http");
+	methodMB.setString("GET");
+        uriMB.setString("/");
+        queryMB.setString("");
+        protoMB.setString("HTTP/1.0");
+        remoteAddrMB.setString("127.0.0.1");
+        remoteHostMB.setString("localhost");
+    }
+
+
+}
---
+++ coyote/src/java/org/apache/coyote/Response.java
@@ -0,0 +1,442 @@
+/*
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Tomcat", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * [Additional notices, if required by prior licensing conditions]
+ *
+ */
+
+
+package org.apache.coyote;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import org.apache.tomcat.util.res.StringManager;
+
+import org.apache.tomcat.util.http.MimeHeaders;
+import org.apache.tomcat.util.http.ContentType;
+
+/**
+ *
+ * @author James Duncan Davidson [duncan@eng.sun.com]
+ * @author Jason Hunter [jch@eng.sun.com]
+ * @author James Todd [gonzo@eng.sun.com]
+ * @author Harish Prabandham
+ * @author Hans Bergsten <hans@gefionsoftware.com>
+ * @author Remy Maucherat
+ */
+public final class Response {
+
+
+    // ----------------------------------------------------------- Constructors
+
+
+    public Response() {
+    }
+
+
+    // ----------------------------------------------------- Instance Variables
+
+
+    /**
+     * Status code.
+     */
+    protected int status = 200;
+
+
+    /**
+     * Response headers.
+     */
+    protected MimeHeaders headers = new MimeHeaders();
+
+
+    /**
+     * Associated output buffer.
+     */
+    protected OutputBuffer outputBuffer;
+
+
+    /**
+     * Notes.
+     */
+    protected Note notes[] = new Note[Constants.MAX_NOTES];
+
+
+    /**
+     * Committed flag.
+     */
+    protected boolean commited = false;
+
+
+    // holds request error exception
+    // set this just once during request processing
+    Exception errorException = null;
+    // holds request error URI
+    String errorURI = null;
+
+
+    // Http specific fileds (remove them ?)
+    protected String contentType = Constants.DEFAULT_CONTENT_TYPE;
+    protected String contentLanguage = null;
+    protected String characterEncoding = Constants.DEFAULT_CHARACTER_ENCODING;
+    protected int contentLength = -1;
+    private Locale locale = Constants.DEFAULT_LOCALE;
+
+
+    /**
+     * Adapter listener.
+     */
+    public AdapterListener listener;
+
+
+    // ------------------------------------------------------------- Properties
+
+
+    public OutputBuffer getOutputBuffer() {
+	return outputBuffer;
+    }
+
+
+    public void setOutputBuffer(OutputBuffer outputBuffer) {
+        this.outputBuffer = outputBuffer;
+    }
+
+
+    public MimeHeaders getMimeHeaders() {
+	return headers;
+    }
+
+
+    public AdapterListener getListener() {
+        return listener;
+    }
+
+
+    public void setAdapterListener(AdapterListener listener) {
+        this.listener = listener;
+    }
+
+
+    // -------------------- Per-Response "notes" --------------------
+
+
+    public final void setNote(int pos, Note value) {
+	notes[pos] = value;
+    }
+
+
+    public final Note getNote(int pos) {
+	return notes[pos];
+    }
+
+
+    // -------------------- Events --------------------
+
+
+    public void sendEvent(AdapterEventCode eventCode, Object param) {
+        if (listener != null) {
+            listener.event(eventCode, param);
+        }
+    }
+
+
+    // -------------------- State --------------------
+
+
+    public int getStatus() {
+        return status;
+    }
+
+    
+    /** 
+     * Set the response status 
+     */ 
+    public void setStatus( int status ) {
+	this.status = status;
+    }
+
+
+    public boolean isCommitted() {
+	return commited;
+    }
+
+
+    public void setCommitted(boolean v) {
+	this.commited = v;
+    }
+
+
+    // -----------------Error State --------------------
+
+
+    /** 
+     * Set the error Exception that occurred during
+     * request processing.
+     */
+    public void setErrorException(Exception ex) {
+	errorException = ex;
+    }
+
+
+    /** 
+     * Get the Exception that occurred during request
+     * processing.
+     */
+    public Exception getErrorException() {
+	return errorException;
+    }
+
+
+    public boolean isExceptionPresent() {
+	return ( errorException != null );
+    }
+
+
+    /** 
+     * Set request URI that caused an error during
+     * request processing.
+     */
+    public void setErrorURI(String uri) {
+	errorURI = uri;
+    }
+
+
+    /** Get the request URI that caused the original error.
+     */
+    public String getErrorURI() {
+	return errorURI;
+    }
+
+
+    // -------------------- Methods --------------------
+    
+    
+    public void reset() throws IllegalStateException {
+        // Reset the headers only if this is the main request,
+        // not for included
+        contentType = Constants.DEFAULT_CONTENT_TYPE;
+        locale = Constants.DEFAULT_LOCALE;
+        characterEncoding = Constants.DEFAULT_CHARACTER_ENCODING;
+        contentLength = -1;
+        status = 200;
+        headers.clear();
+        
+	// Force the PrintWriter to flush its data to the output
+	// stream before resetting the output stream
+	//
+	// Reset the stream
+	if( commited ) {
+	    //String msg = sm.getString("servletOutputStreamImpl.reset.ise"); 
+	    throw new IllegalStateException(/*msg*/);
+	}
+	outputBuffer.reset();
+    }
+
+
+    public void finish() throws IOException {
+	outputBuffer.close();
+    }
+
+
+    // -------------------- Headers --------------------
+    public boolean containsHeader(String name) {
+	return headers.getHeader(name) != null;
+    }
+
+
+    public void setHeader(String name, String value) {
+	char cc=name.charAt(0);
+	if( cc=='C' || cc=='c' ) {
+	    if( checkSpecialHeader(name, value) )
+		return;
+	}
+	headers.setValue(name).setString( value);
+    }
+
+
+    public void addHeader(String name, String value) {
+	char cc=name.charAt(0);
+	if( cc=='C' || cc=='c' ) {
+	    if( checkSpecialHeader(name, value) )
+		return;
+	}
+	headers.addValue(name).setString( value );
+    }
+
+    
+    /** 
+     * Set internal fields for special header names. 
+     * Called from set/addHeader.
+     * Return true if the header is special, no need to set the header.
+     */
+    private boolean checkSpecialHeader( String name, String value) {
+	// XXX Eliminate redundant fields !!!
+	// ( both header and in special fields )
+	if( name.equalsIgnoreCase( "Content-Type" ) ) {
+	    setContentType( value );
+	    return true;
+	}
+	if( name.equalsIgnoreCase( "Content-Length" ) ) {
+	    try {
+		int cL=Integer.parseInt( value );
+		setContentLength( cL );
+		return true;
+	    } catch( NumberFormatException ex ) {
+		// Do nothing - the spec doesn't have any "throws" 
+		// and the user might know what he's doing
+		return false;
+	    }
+	}
+	if( name.equalsIgnoreCase( "Content-Language" ) ) {
+	    // XXX XXX Need to construct Locale or something else
+	}
+	return false;
+    }
+
+
+    /** Signal that we're done with the headers, and body will follow.
+     *  Any implementation needs to notify ContextManager, to allow
+     *  interceptors to fix headers.
+     */
+    public void sendHeaders() throws IOException {
+	commited = true;
+    }
+
+
+    // -------------------- I18N --------------------
+
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    /**
+     * Called explicitely by user to set the Content-Language and
+     * the default encoding
+     */
+    public void setLocale(Locale locale) {
+        if (locale == null) {
+            return;  // throw an exception?
+        }
+
+        // Save the locale for use by getLocale()
+        this.locale = locale;
+
+        // Set the contentLanguage for header output
+        contentLanguage = locale.getLanguage();
+
+	// only one header !
+	headers.setValue("Content-Language").setString( contentLanguage);
+    }
+
+    public String getCharacterEncoding() {
+	return characterEncoding;
+    }
+
+    public void setContentType(String contentType) {
+	this.contentType = contentType;
+	String encoding = ContentType.getCharsetFromContentType(contentType);
+        if (encoding != null) {
+	    characterEncoding = encoding;
+        }
+	headers.setValue("Content-Type").setString( contentType);
+    }
+
+    public String getContentType() {
+	return contentType;
+    }
+    
+    public void setContentLength(int contentLength) {
+	this.contentLength = contentLength;
+	headers.setValue("Content-Length").setInt(contentLength);
+    }
+
+    public int getContentLength() {
+	return contentLength;
+    }
+
+
+    /** 
+     * Write a chunk of bytes.
+     */
+    public void doWrite(byte buffer[], int pos, int count)
+        throws IOException {
+        outputBuffer.doWrite(buffer, pos, count);
+    }
+
+
+    // --------------------
+    
+    public void recycle() {
+
+	contentType = Constants.DEFAULT_CONTENT_TYPE;
+	contentLanguage = null;
+        locale = Constants.DEFAULT_LOCALE;
+	characterEncoding = Constants.DEFAULT_CHARACTER_ENCODING;
+	contentLength = -1;
+	status = 200;
+	commited = false;
+	errorException = null;
+	errorURI = null;
+	headers.clear();
+
+        for (int i=0; i < Constants.MAX_NOTES; i++) {
+            if (notes[i] != null)
+                notes[i].recycle();
+        }
+
+    }
+
+}
