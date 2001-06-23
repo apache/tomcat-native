85db87cac65183ce056c2e2230a9ab0b42e4055b
Costin Manolache <costin@apache.org>
2001-06-23 05:52:12 +0000

    Added a new target to build ajp14 for tomcat33.
    Added few more defaults for properties ( based on the current dir layout,
    assuming the same dir is used to check out the repositories ). The
    properties file can override that, but it's good to have defaults.
    
    Added a small mkdir in the config script, cvs update -dP will remove
    the build/unix dir.
    
    Also made "optimize" and "debug" settable properties, consistent with
    jakarta-tomcat and other projects.
    
    Congrats for the ./configure scripts, it's amazing. Tommorow I'll play a bit
    with ajp14, so far it compiles without problems.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@295485 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native/buildconf.sh
+++ jk/native/buildconf.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+# if cvs -dP the dir will not exist. Alternative: create a dummy file in it
+mkdir script/tool/unix
 echo "libtoolize --force --automake"
 libtoolize --force --automake
 echo "automake -a --foreign -i"
