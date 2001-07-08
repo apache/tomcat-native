386dd9087d8a86fdc4aa5a66079aa339aaab485f
Pier Fumagalli <pier@apache.org>
2001-07-08 03:32:07 +0000

    First part of the new build method. More tomorrow morning. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@295557 13f79535-47bb-0310-9956-ffa450edef68

--- webapp/buildconf.sh
+++ webapp/buildconf.sh
@@ -6,4 +6,9 @@
 # the "configure.in". If you check these sources out of CVS,
 # you will need to execute this script first.
 
+if [ ! -d ./apr ]
+then
+    mkdir apr
+    echo "Don't a forget to put a copy of the APR sources in `pwd`/apr/"
+fi
 autoconf
