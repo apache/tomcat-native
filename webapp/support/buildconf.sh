0df5ae76222b363a4fd6108d0d7a96484a25068b
Pier Fumagalli <pier@apache.org>
2001-07-13 02:05:11 +0000

    Added support file for building Java code. Moved buildconf in the support directory.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@295576 13f79535-47bb-0310-9956-ffa450edef68

---
+++ webapp/support/buildconf.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Author Jon S. Stevens <mailto:jon@latchkey.com>
+# Author Pier Fumagalli <pier.fumagalli@sun.com>
+#
+# This script is used to build the "configure" script from
+# the "configure.in". If you check these sources out of CVS,
+# you will need to execute this script first.
+
+if [ ! -f ./configure.in ]
+then
+    echo "Cannot find \"configure.in\" file."
+    exit 1
+fi
+
+if [ ! -d ./apr ]
+then
+    mkdir apr
+    echo "Don't a forget to put a copy of the APR sources in `pwd`/apr/"
+fi
+autoconf
