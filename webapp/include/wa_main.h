4395b8f80150ecd7c20064c7e150d859fc28bb74
Pier Fumagalli <pier@apache.org>
2001-07-13 02:18:18 +0000

    Now a module _MUST_ provide a wa_log(...) call to log messages outside request-processing. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@295587 13f79535-47bb-0310-9956-ffa450edef68

--- webapp/include/wa_main.h
+++ webapp/include/wa_main.h
@@ -214,6 +214,19 @@ const char *wa_deploy(wa_application *a,
 void wa_debug(const char *f, const int l, const char *fmt, ...);
 
 /**
+ * Report an error to the web-server log file.
+ * <br>
+ * NOTE: This function is _NOT_ defined in the WebApp library. It _MUST_ be
+ * defined and implemented within the web-server module.
+ *
+ * @param f The file where this function was called.
+ * @param l The line number where this function was called.
+ * @param fmt The format string of the debug message (printf style).
+ * @param others The parameters to the format string.
+ */
+void wa_log(const char *f, const int l, const char *fmt, ...);
+
+/**
  * The WebApp library memory pool.
  */
 extern apr_pool_t *wa_pool;
