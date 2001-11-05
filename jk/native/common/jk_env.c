be74e94c8506f8aa4a670df2e7dd1f583ebc22cc
Costin Manolache <costin@apache.org>
2001-11-05 23:19:29 +0000

    A small change in the mechanism used to register workers.
    Jk is 'object oriented', but so far the only 'interface' with multiple implementations
    are 'jk_worker'. ( well, also the server adapter - but at runtime you have a single
    one, depending on what server you use ).
    
    In order to 'play' with different channels and optimizations _without_ affecting the
    stability of jk we need to enhance the config mechanism to support other object types.
    ( for example different types of channel ).
    
    This is also a first step in using APR - again, the current code will remain as
    the 'stable' implementation ( after some minimal refactoring ), while objects using
    APR can be configured-in at runtime ( on Apache2.0 or if apr is available ).
    
    Before this, we had an array of workers that was used to create the worker_list.
    This changes add a 'registerFactory' method that could be used for any kind of
    object  ( right now we don't check for type, the factory method will be able to do
    this ). This is similar with how hooks are registered in 2.0 ( using explicit method
    calls )
    
    An additional benefit is that we can split jk in multiple modules - the 'main' module
    will contain the common code, and additional modules could plug in alternative
    workers, channels, loggers, etc.
    
    The jk_env will grow to provide a simpler access to loggers, pools, etc.
    
    The code change is minimal - and everything seems to work fine, but it would be nice if
    someone can verify.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@295916 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jk/native/common/jk_env.c
@@ -0,0 +1,130 @@
+/* ========================================================================= *
+ *                                                                           *
+ *                 The Apache Software License,  Version 1.1                 *
+ *                                                                           *
+ *          Copyright (c) 1999-2001 The Apache Software Foundation.          *
+ *                           All rights reserved.                            *
+ *                                                                           *
+ * ========================================================================= *
+ *                                                                           *
+ * Redistribution and use in source and binary forms,  with or without modi- *
+ * fication, are permitted provided that the following conditions are met:   *
+ *                                                                           *
+ * 1. Redistributions of source code  must retain the above copyright notice *
+ *    notice, this list of conditions and the following disclaimer.          *
+ *                                                                           *
+ * 2. Redistributions  in binary  form  must  reproduce the  above copyright *
+ *    notice,  this list of conditions  and the following  disclaimer in the *
+ *    documentation and/or other materials provided with the distribution.   *
+ *                                                                           *
+ * 3. The end-user documentation  included with the redistribution,  if any, *
+ *    must include the following acknowlegement:                             *
+ *                                                                           *
+ *       "This product includes  software developed  by the Apache  Software *
+ *        Foundation <http://www.apache.org/>."                              *
+ *                                                                           *
+ *    Alternately, this acknowlegement may appear in the software itself, if *
+ *    and wherever such third-party acknowlegements normally appear.         *
+ *                                                                           *
+ * 4. The names  "The  Jakarta  Project",  "Jk",  and  "Apache  Software     *
+ *    Foundation"  must not be used  to endorse or promote  products derived *
+ *    from this  software without  prior  written  permission.  For  written *
+ *    permission, please contact <apache@apache.org>.                        *
+ *                                                                           *
+ * 5. Products derived from this software may not be called "Apache" nor may *
+ *    "Apache" appear in their names without prior written permission of the *
+ *    Apache Software Foundation.                                            *
+ *                                                                           *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES *
+ * INCLUDING, BUT NOT LIMITED TO,  THE IMPLIED WARRANTIES OF MERCHANTABILITY *
+ * AND FITNESS FOR  A PARTICULAR PURPOSE  ARE DISCLAIMED.  IN NO EVENT SHALL *
+ * THE APACHE  SOFTWARE  FOUNDATION OR  ITS CONTRIBUTORS  BE LIABLE  FOR ANY *
+ * DIRECT,  INDIRECT,   INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR  CONSEQUENTIAL *
+ * DAMAGES (INCLUDING,  BUT NOT LIMITED TO,  PROCUREMENT OF SUBSTITUTE GOODS *
+ * OR SERVICES;  LOSS OF USE,  DATA,  OR PROFITS;  OR BUSINESS INTERRUPTION) *
+ * HOWEVER CAUSED AND  ON ANY  THEORY  OF  LIABILITY,  WHETHER IN  CONTRACT, *
+ * STRICT LIABILITY, OR TORT  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN *
+ * ANY  WAY  OUT OF  THE  USE OF  THIS  SOFTWARE,  EVEN  IF  ADVISED  OF THE *
+ * POSSIBILITY OF SUCH DAMAGE.                                               *
+ *                                                                           *
+ * ========================================================================= *
+ *                                                                           *
+ * This software  consists of voluntary  contributions made  by many indivi- *
+ * duals on behalf of the  Apache Software Foundation.  For more information *
+ * on the Apache Software Foundation, please see <http://www.apache.org/>.   *
+ *                                                                           *
+ * ========================================================================= */
+ 
+#include "jk_env.h"
+
+/* Singleton for now */
+jk_env_t *jk_env_singleton;
+
+/* Private methods 
+*/
+static jk_env_initEnv( jk_env_t *env, char *id );
+static jk_env_objectFactory_t *jk_env_getFactory(jk_env_t *env, 
+						 char *type,
+						 char *name );
+static void jk_env_registerFactory(jk_env_t *env, 
+				   char *type,
+				   char *name, 
+				   jk_env_objectFactory_t fact);
+
+/* XXX We should have one env per thread to avoid sync problems. 
+   The env will provide access to pools, etc 
+*/
+
+jk_env_t JK_METHOD *jk_env_getEnv( char *id ) {
+  if( jk_env_singleton == NULL ) {
+    jk_env_singleton=(jk_env_t *)malloc( sizeof( jk_env_t ));
+    jk_env_initEnv( (jk_env_t *)jk_env_singleton, id );
+  }
+  return jk_env_singleton;
+}
+
+/* ==================== Implementation ==================== */
+
+static jk_env_initEnv( jk_env_t *env, char *id ) {
+  /*   env->logger=NULL; */
+  /*   map_alloc( & env->properties ); */
+  env->getFactory= jk_env_getFactory; 
+  env->registerFactory= jk_env_registerFactory; 
+  map_alloc( & env->_registry);
+  jk_registry_init(env);
+
+}
+
+static jk_env_objectFactory_t *jk_env_getFactory(jk_env_t *env, 
+						 char *type,
+						 char *name )
+{
+  jk_env_objectFactory_t *result;
+
+  if( type==NULL || name==NULL ) {
+    // throw NullPointerException
+    return NULL;
+  }
+
+  /** XXX add check for the type */
+  result=(jk_env_objectFactory_t *)map_get( env->_registry, name, NULL );
+  return result;
+}
+
+static void jk_env_registerFactory(jk_env_t *env, 
+ 				  char *type,
+				  char *name, 
+				  jk_env_objectFactory_t fact)
+{
+  void *old;
+  
+  map_put( env->_registry, name, fact, &old );
+}
+
+
+
+
+
+
+
+
---
+++ jk/native/common/jk_registry.c
@@ -0,0 +1,136 @@
+/* ========================================================================= *
+ *                                                                           *
+ *                 The Apache Software License,  Version 1.1                 *
+ *                                                                           *
+ *          Copyright (c) 1999-2001 The Apache Software Foundation.          *
+ *                           All rights reserved.                            *
+ *                                                                           *
+ * ========================================================================= *
+ *                                                                           *
+ * Redistribution and use in source and binary forms,  with or without modi- *
+ * fication, are permitted provided that the following conditions are met:   *
+ *                                                                           *
+ * 1. Redistributions of source code  must retain the above copyright notice *
+ *    notice, this list of conditions and the following disclaimer.          *
+ *                                                                           *
+ * 2. Redistributions  in binary  form  must  reproduce the  above copyright *
+ *    notice,  this list of conditions  and the following  disclaimer in the *
+ *    documentation and/or other materials provided with the distribution.   *
+ *                                                                           *
+ * 3. The end-user documentation  included with the redistribution,  if any, *
+ *    must include the following acknowlegement:                             *
+ *                                                                           *
+ *       "This product includes  software developed  by the Apache  Software *
+ *        Foundation <http://www.apache.org/>."                              *
+ *                                                                           *
+ *    Alternately, this acknowlegement may appear in the software itself, if *
+ *    and wherever such third-party acknowlegements normally appear.         *
+ *                                                                           *
+ * 4. The names  "The  Jakarta  Project",  "Jk",  and  "Apache  Software     *
+ *    Foundation"  must not be used  to endorse or promote  products derived *
+ *    from this  software without  prior  written  permission.  For  written *
+ *    permission, please contact <apache@apache.org>.                        *
+ *                                                                           *
+ * 5. Products derived from this software may not be called "Apache" nor may *
+ *    "Apache" appear in their names without prior written permission of the *
+ *    Apache Software Foundation.                                            *
+ *                                                                           *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES *
+ * INCLUDING, BUT NOT LIMITED TO,  THE IMPLIED WARRANTIES OF MERCHANTABILITY *
+ * AND FITNESS FOR  A PARTICULAR PURPOSE  ARE DISCLAIMED.  IN NO EVENT SHALL *
+ * THE APACHE  SOFTWARE  FOUNDATION OR  ITS CONTRIBUTORS  BE LIABLE  FOR ANY *
+ * DIRECT,  INDIRECT,   INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR  CONSEQUENTIAL *
+ * DAMAGES (INCLUDING,  BUT NOT LIMITED TO,  PROCUREMENT OF SUBSTITUTE GOODS *
+ * OR SERVICES;  LOSS OF USE,  DATA,  OR PROFITS;  OR BUSINESS INTERRUPTION) *
+ * HOWEVER CAUSED AND  ON ANY  THEORY  OF  LIABILITY,  WHETHER IN  CONTRACT, *
+ * STRICT LIABILITY, OR TORT  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN *
+ * ANY  WAY  OUT OF  THE  USE OF  THIS  SOFTWARE,  EVEN  IF  ADVISED  OF THE *
+ * POSSIBILITY OF SUCH DAMAGE.                                               *
+ *                                                                           *
+ * ========================================================================= *
+ *                                                                           *
+ * This software  consists of voluntary  contributions made  by many indivi- *
+ * duals on behalf of the  Apache Software Foundation.  For more information *
+ * on the Apache Software Foundation, please see <http://www.apache.org/>.   *
+ *                                                                           *
+ * ========================================================================= */
+
+#include "jk_logger.h"
+#include "jk_pool.h"
+#include "jk_service.h"
+#include "jk_env.h"
+
+/***************************************************************************
+ * Description: Worker list                                                *
+ * Version:     $Revision$                                           *
+ ***************************************************************************/
+
+/** Static declarations for all 'hardcoded' modules. This is a hack, 
+ *  needed to initialize the static 'type system' for jk. 
+ *
+ *  A better solution would use 'introspection' and dlsyms to locate the 
+ *  factory, using the common naming schema. This is a bit harder to implement
+ *  ( we can do it later ) and may have problems on some buggy OSs ( I assume
+ *  most decent systems support .so/.dll and symbols, but you can never know )
+ * 
+ *  Both solutions could be used in paralel - for start we'll use this
+ *  hack and static registration. Don't put anything else here, just 
+ *  static declarations for the factory methods and the registrerFactory() call.
+ * 
+ * (based on jk_worker_list.h )
+ * @author:      Gal Shachor <shachor@il.ibm.com>                           *
+ * @author:      Henri Gomez <hgomez@slib.fr>                               *
+ * @author:      Costin Manolache
+ *  
+ */
+
+int JK_METHOD ajp12_worker_factory(jk_worker_t **w,
+                                   const char *name,
+                                   jk_logger_t *l);
+
+int JK_METHOD ajp13_worker_factory(jk_worker_t **w,
+                                   const char *name,
+                                   jk_logger_t *l);
+
+int JK_METHOD ajp14_worker_factory(jk_worker_t **w,
+                                   const char *name,
+                                   jk_logger_t *l);
+
+int JK_METHOD lb_worker_factory(jk_worker_t **w,
+                                const char *name,
+                                jk_logger_t *l);
+
+#ifdef HAVE_JNI
+int JK_METHOD jni_worker_factory(jk_worker_t **w,
+                                 const char *name,
+                                 jk_logger_t *l);
+#endif
+
+/**
+ *   Init the components that we compile in by default. 
+ *   In future we should have a more flexible mechanism that would allow 
+ *   different server modules to load and register various jk objects, or 
+ *   even have jk load it's own modules from .so files.
+ * 
+ *   For now the static registrartion should work.
+ */
+int JK_METHOD jk_registry_init(jk_env_t *env) {
+  if( env==NULL ) {
+    /* XXX do something ! */
+    printf("jk_registry_init: Assertion failed, env==NULL\n" );
+    return;
+  }
+  env->registerFactory( env, "worker", "ajp12", &ajp12_worker_factory );
+  env->registerFactory( env, "worker", "ajp13", &ajp13_worker_factory );
+  env->registerFactory( env, "worker", "ajp14", &ajp14_worker_factory );
+  env->registerFactory( env, "worker", "lb",    &lb_worker_factory );
+#ifdef HAVE_JNI
+  env->registerFactory( env, "worker", "jni",   &jni_worker_factory );
+#endif
+
+  /*
+  env->registerFactory( env, "channel", "socket",   &jk_channel_socket_factory );
+  */
+  /* Add all other factories here */
+}
+
