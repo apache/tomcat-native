843bee2d9a7fc32c3b7907b74431c0a0fc8acdce
Costin Manolache <costin@apache.org>
2001-12-01 22:29:23 +0000

    Logger interface, modified to make jk_log a virtual method ( so we're not statically bound to file logger )
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@296029 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jk/native2/include/jk_logger.h
@@ -0,0 +1,129 @@
+/* ========================================================================= *
+ *                                                                           *
+ *                 The Apache Software License,  Version 1.1                 *
+ *                                                                           *
+ *          Copyright (c) 1999-2001 The Apache Software Foundation.          *
+ *                           All rights reserved.                            *
+ *                                                                           *
+ * ========================================================================= *
+ *                                                                           *
+ * Redistribution and use in source and binary forms,  with or without modi- *
+ * fication, are permitted provided that the following conditions are met:   *
+ *                                                                           *
+ * 1. Redistributions of source code  must retain the above copyright notice *
+ *    notice, this list of conditions and the following disclaimer.          *
+ *                                                                           *
+ * 2. Redistributions  in binary  form  must  reproduce the  above copyright *
+ *    notice,  this list of conditions  and the following  disclaimer in the *
+ *    documentation and/or other materials provided with the distribution.   *
+ *                                                                           *
+ * 3. The end-user documentation  included with the redistribution,  if any, *
+ *    must include the following acknowlegement:                             *
+ *                                                                           *
+ *       "This product includes  software developed  by the Apache  Software *
+ *        Foundation <http://www.apache.org/>."                              *
+ *                                                                           *
+ *    Alternately, this acknowlegement may appear in the software itself, if *
+ *    and wherever such third-party acknowlegements normally appear.         *
+ *                                                                           *
+ * 4. The names  "The  Jakarta  Project",  "Jk",  and  "Apache  Software     *
+ *    Foundation"  must not be used  to endorse or promote  products derived *
+ *    from this  software without  prior  written  permission.  For  written *
+ *    permission, please contact <apache@apache.org>.                        *
+ *                                                                           *
+ * 5. Products derived from this software may not be called "Apache" nor may *
+ *    "Apache" appear in their names without prior written permission of the *
+ *    Apache Software Foundation.                                            *
+ *                                                                           *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES *
+ * INCLUDING, BUT NOT LIMITED TO,  THE IMPLIED WARRANTIES OF MERCHANTABILITY *
+ * AND FITNESS FOR  A PARTICULAR PURPOSE  ARE DISCLAIMED.  IN NO EVENT SHALL *
+ * THE APACHE  SOFTWARE  FOUNDATION OR  ITS CONTRIBUTORS  BE LIABLE  FOR ANY *
+ * DIRECT,  INDIRECT,   INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR  CONSEQUENTIAL *
+ * DAMAGES (INCLUDING,  BUT NOT LIMITED TO,  PROCUREMENT OF SUBSTITUTE GOODS *
+ * OR SERVICES;  LOSS OF USE,  DATA,  OR PROFITS;  OR BUSINESS INTERRUPTION) *
+ * HOWEVER CAUSED AND  ON ANY  THEORY  OF  LIABILITY,  WHETHER IN  CONTRACT, *
+ * STRICT LIABILITY, OR TORT  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN *
+ * ANY  WAY  OUT OF  THE  USE OF  THIS  SOFTWARE,  EVEN  IF  ADVISED  OF THE *
+ * POSSIBILITY OF SUCH DAMAGE.                                               *
+ *                                                                           *
+ * ========================================================================= *
+ *                                                                           *
+ * This software  consists of voluntary  contributions made  by many indivi- *
+ * duals on behalf of the  Apache Software Foundation.  For more information *
+ * on the Apache Software Foundation, please see <http://www.apache.org/>.   *
+ *                                                                           *
+ * ========================================================================= */
+
+/***************************************************************************
+ * Description: Logger object definitions                                  *
+ * Author:      Gal Shachor <shachor@il.ibm.com>                           *
+ * Version:     $Revision$                                           *
+ ***************************************************************************/
+
+#ifndef JK_LOGGER_H
+#define JK_LOGGER_H
+
+#include "jk_global.h"
+#include "jk_map.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+typedef struct jk_logger jk_logger_t;
+
+/* Logger object.
+ *  XXX level should be moved per component ( to control the generation of messages ),
+ *  the level param in the param should be used only as information ( to be displayed
+ *  in the log ).
+ */
+struct jk_logger {
+    void *logger_private;
+    int  level;
+
+    void (JK_METHOD *setProperty)(jk_logger_t *_this,
+                                  const char *name,
+                                  const char *value );
+
+    char *(JK_METHOD *getProperty)(jk_logger_t *_this,
+                                   const char *name,
+                                   const char *def );
+
+    void (JK_METHOD *open)( jk_logger_t *_this,
+                            jk_map_t *properties );
+
+    void (JK_METHOD *close)( jk_logger_t *_this );
+
+    int (JK_METHOD *log)(jk_logger_t *_this,
+                         int level,
+                         const char *what);
+
+    int (JK_METHOD *jkLog)(jk_logger_t *_this,
+                           const char *file,
+                           int line,
+                           int level,
+                           const char *fmt, ...);
+
+};
+
+#define JK_LOG_DEBUG_LEVEL 0
+#define JK_LOG_INFO_LEVEL  1
+#define JK_LOG_ERROR_LEVEL 2
+#define JK_LOG_EMERG_LEVEL 3
+
+#define JK_LOG_DEBUG_VERB   "debug"
+#define JK_LOG_INFO_VERB    "info"
+#define JK_LOG_ERROR_VERB   "error"
+#define JK_LOG_EMERG_VERB   "emerg"
+
+#define JK_LOG_DEBUG __FILE__,__LINE__,JK_LOG_DEBUG_LEVEL
+#define JK_LOG_INFO  __FILE__,__LINE__,JK_LOG_INFO_LEVEL
+#define JK_LOG_ERROR __FILE__,__LINE__,JK_LOG_ERROR_LEVEL
+#define JK_LOG_EMERG __FILE__,__LINE__,JK_LOG_EMERG_LEVEL
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* JK_LOGGER_H */
