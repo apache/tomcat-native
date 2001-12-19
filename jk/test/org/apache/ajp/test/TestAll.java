614426954049da1f1f0e5ed5a37a26c72fa98fa8
Costin Manolache <costin@apache.org>
2001-12-19 19:21:55 +0000

    Moved test files to separate dir.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@296181 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jk/test/org/apache/ajp/test/TestAll.java
@@ -0,0 +1,25 @@
+package org.apache.ajp.test;
+
+// junit
+import junit.framework.*;
+
+// ajp
+import org.apache.ajp.*;
+
+public class TestAll extends TestCase {
+
+    public TestAll(String testName) {
+        super(testName);
+    }
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite();
+        suite.addTest(TestAjp13.suite());
+        return suite;
+    }
+
+    public static void main(String args[]) {
+        String[] testCaseName = { TestAll.class.getName() };
+        junit.textui.TestRunner.main(testCaseName);
+    }
+}
