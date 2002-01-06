389b0412cbaacfb034e51508f54325bd9853ade6
Costin Manolache <costin@apache.org>
2002-01-06 08:40:44 +0000

    Added unix socket native methods. ( JFC already commited another impl, and it seems better - so I'll probably replace mine )
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@296220 13f79535-47bb-0310-9956-ffa450edef68

--- jk/java/org/apache/jk/apr/AprImpl.java
+++ jk/java/org/apache/jk/apr/AprImpl.java
@@ -7,18 +7,55 @@ package org.apache.jk.apr;
  */
 public class AprImpl {
 
-    private native void initApr();
-    
-    private void loadNative() {
-
-
-    }
-    
-    /** Temp - testing only, will be moved to separate file
+    /** Initialize APR
      */
-    public void main(String args[] ) {
-        
-    }
-        
+    public native int initialize();
 
+    public native int terminate();
+
+    public native long poolCreate(long parentPool);
+
+    public native long poolClear(long pool);
+
+    public native long unSocketClose( long pool, long socket, int type );
+
+    /** Create a unix socket and start listening. 
+     *  @param file the name of the socket
+     *  @param bl backlog
+     */
+    public native long unSocketListen( long pool, String file, int bl );
+    
+    /** Create a unix socket and connect. 
+     *  @param file the name of the socket
+     *  @param bl backlog
+     */
+    public native long unSocketConnect( long pool, String file );
+
+    /** Accept a connection.
+     */
+    public native long unAccept( long pool, long unListeningSocket );
+
+    public native int unRead( long pool, long unSocket,
+                                byte buf[], int off, int len );
+
+    public native int unWrite( long pool, long unSocket,
+                                byte buf[], int off, int len );
+
+    public void loadNative(String libPath) {
+        try {
+            if( libPath==null )
+                libPath="jni_connect";
+            // XXX use load() for full path
+            if( libPath.indexOf( "/" ) >=0 ||
+                libPath.indexOf( "\\" ) >=0 ) {
+                System.load( libPath );
+            } else {
+                System.loadLibrary( libPath );
+            }
+        } catch( RuntimeException ex ) {
+            ex.printStackTrace();
+        }
+    }
+    
+    
 }
