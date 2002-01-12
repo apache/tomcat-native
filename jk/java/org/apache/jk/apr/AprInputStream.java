716e0044ddf81e06814d70dc92e11fabd2d8be79
Costin Manolache <costin@apache.org>
2002-01-12 04:00:15 +0000

    Abstractions for the apr is, os, socket. Initial version based on JFC's unix socket ( which will eventually be part of the standard apr ).
    Those are very early, and jk is using the AprImpl directly. In time
    we should make them implement completely the java interfaces ( for
    example use Socket, etc ).
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@296252 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jk/java/org/apache/jk/apr/AprInputStream.java
@@ -0,0 +1,103 @@
+/*
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Tomcat", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * [Additional notices, if required by prior licensing conditions]
+ *
+ */
+package org.apache.jk.apr;
+
+import java.io.*;
+
+import java.net.*;
+import java.util.*;
+
+import org.apache.tomcat.util.buf.*;
+import org.apache.tomcat.util.http.*;
+
+import org.apache.tomcat.util.threads.*;
+
+/**
+ * Provide an InputStream for the native socket.
+ * 
+ *
+ * @author Jean-Frederic Clere
+ * @author Costin Manolache
+ */
+class AprInputStream extends InputStream {
+    int type; // right now only LOCAL
+    private int fd; // From openNative.
+    private AprImpl apr;
+    long pool;
+
+    /**
+     * Create an InputStream using apr for I/O. This can
+     * support multiple 'styles' ( unix socket, apr tcp socket,
+     * etc ). We could split it - the interfaces are highly
+     * experimental for now.
+     */ 
+    public AprInputStream(AprImpl apr, long pool, int type, int fd) {
+        this.apr=apr;
+        this.fd = fd;
+        this.pool=pool;
+        this.type=type;
+    }
+
+    public int read() throws IOException {
+        byte buf [] = new byte[1];
+        if ( apr.unRead( pool, fd, buf, 0, 1) < 0)
+            throw new IOException();
+        return 0xff & buf[0];
+    }
+}
