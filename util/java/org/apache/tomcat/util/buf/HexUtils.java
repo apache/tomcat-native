8c50eba0af935813bedfff0830bca0848d911a85
Remy Maucherat <remm@apache.org>
2002-01-23 18:00:11 +0000

    - Fix possible build problem, reported by Kevin Seguin.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@296305 13f79535-47bb-0310-9956-ffa450edef68

--- util/java/org/apache/tomcat/util/buf/HexUtils.java
+++ util/java/org/apache/tomcat/util/buf/HexUtils.java
@@ -104,8 +104,9 @@ public final class HexUtils {
      * Table for DEC to HEX byte translation.
      */
     public static final byte[] HEX = 
-    { '0', '1', '2', '3', '4', '5', '6', 
-      '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+    { (byte) '0', (byte) '1', (byte) '2', (byte) '3', (byte) '4', (byte) '5', 
+      (byte) '6', (byte) '7', (byte) '8', (byte) '9', (byte) 'a', (byte) 'b', 
+      (byte) 'c', (byte) 'd', (byte) 'e', (byte) 'f' };
 
 
     /**
