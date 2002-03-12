0ba5ee346a5251b3ba9d669f0eaa4b14c2c439fc
Remy Maucherat <remm@apache.org>
2002-03-12 20:21:21 +0000

    - Port change from the HTTP connector. - Revert the patch for 6600, which was causing URL encoding to fail. - Bug 6600 will be converted to an enhancement request (the spec doesn't   require this to work).
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@296525 13f79535-47bb-0310-9956-ffa450edef68

--- coyote/src/java/org/apache/coyote/tomcat4/CoyoteResponse.java
+++ coyote/src/java/org/apache/coyote/tomcat4/CoyoteResponse.java
@@ -1181,7 +1181,7 @@ public class CoyoteResponse
         HttpSession session = hreq.getSession(false);
         if (session == null)
             return (false);
-        if (!hreq.isRequestedSessionIdFromURL())
+        if (hreq.isRequestedSessionIdFromCookie())
             return (false);
 
         // Is this a valid absolute URL?
