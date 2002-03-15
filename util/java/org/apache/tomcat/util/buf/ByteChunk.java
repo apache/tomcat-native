63053b5f6f5f7111159285492009bd22bb1b6a90
Remy Maucherat <remm@apache.org>
2002-03-15 05:28:35 +0000

    - Made the offsets relative to the start of the chunk (instead of relative to the   start of the internal byte buffer). This looks more consistent with the other indexOf. - Also fix an off-by-one but in the matching algorithm. - If this patch is not ok, let me know.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@296534 13f79535-47bb-0310-9956-ffa450edef68

--- util/java/org/apache/tomcat/util/buf/ByteChunk.java
+++ util/java/org/apache/tomcat/util/buf/ByteChunk.java
@@ -572,16 +572,16 @@ public final class ByteChunk implements Cloneable, Serializable {
 	char first=src.charAt( srcOff );
 
 	// Look for first char 
-	int srcEnd=srcOff + srcLen;
-	
-	for( int i=myOff; i< end - srcLen ; i++ ) {
+	int srcEnd = srcOff + srcLen;
+        
+	for( int i=myOff+start; i< end - srcLen ; i++ ) {
 	    if( buff[i] != first ) continue;
 	    // found first char, now look for a match
-	    int myPos=i+1;
-	    for( int srcPos=srcOff; srcPos< srcEnd; ) {
-		if( buff[myPos++] != src.charAt( srcPos++ ))
+            int myPos=i+1;
+	    for( int srcPos=srcOff + 1; srcPos< srcEnd; ) {
+                if( buff[myPos++] != src.charAt( srcPos++ ))
 		    break;
-		if( srcPos==srcEnd ) return i; // found it
+                if( srcPos==srcEnd ) return i-start; // found it
 	    }
 	}
 	return -1;
