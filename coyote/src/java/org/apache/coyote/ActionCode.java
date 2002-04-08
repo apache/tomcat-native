f30b11916420b8cdd96ce8c689ec70abacdbadad
Costin Manolache <costin@apache.org>
2002-04-08 22:46:58 +0000

    Add the 2 callbacks that are typically needed - one to compute the remote host and the other for ssl attributes.
    Both are expensive and should be done on-demand
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@296690 13f79535-47bb-0310-9956-ffa450edef68

--- coyote/src/java/org/apache/coyote/ActionCode.java
+++ coyote/src/java/org/apache/coyote/ActionCode.java
@@ -90,9 +90,13 @@ public final class ActionCode {
 
     public static final ActionCode ACTION_STOP = new ActionCode();
 
-    /** Compute request attribute
+    /** Callback for lazy evaluation - extract the remote host address
      */
-    public static final ActionCode ACTION_REQ_ATTRIBUTE = new ActionCode();
+    public static final ActionCode ACTION_REQ_HOST_ATTRIBUTE = new ActionCode();
+
+    /** Callback for lazy evaluation - extract the SSL-related attributes
+     */
+    public static final ActionCode ACTION_REQ_SSL_ATTRIBUTE = new ActionCode();
 
 
     // ----------------------------------------------------------- Constructors
