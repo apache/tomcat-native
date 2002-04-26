4f889431494d236afcf7541b883f5f889d051aca
William Barker <billbarker@apache.org>
2002-04-26 03:26:21 +0000

    Note to self: Remember to take debugging statements out before ci
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@296956 13f79535-47bb-0310-9956-ffa450edef68

--- http11/src/java/org/apache/coyote/http11/Http11Processor.java
+++ http11/src/java/org/apache/coyote/http11/Http11Processor.java
@@ -374,13 +374,11 @@ public class Http11Processor implements Processor, ActionHook {
         int soTimeout = socket.getSoTimeout();
         boolean keptAlive = false;
         socket.setSoTimeout(timeout);
-	log.warn("soTimeout = " + soTimeout + " timeout = " + timeout);
         while (started && !error && keepAlive) {
             try {
                 if( keptAlive && soTimeout > 0 ) {
                     socket.setSoTimeout(soTimeout);
                 }
-		log.warn("timeout = " + socket.getSoTimeout());
                 inputBuffer.parseRequestLine();
                 keptAlive = true;
                 socket.setSoTimeout(timeout);
