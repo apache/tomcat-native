edfe58334bc0a41b397c58a3b74b02797dd69b8d
Pier Fumagalli <pier@apache.org>
2002-04-29 19:22:04 +0000

    Check for AUTOCONF version as well now before creating the CONFIGURE script. PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@296973 13f79535-47bb-0310-9956-ffa450edef68

--- webapp/support/buildconf.sh
+++ webapp/support/buildconf.sh
@@ -58,32 +58,77 @@
 
 # ------------------------------------------------------------------------- #
 # Author Pier Fumagalli <mailto:pier@betaversion.org>
-# Author Jon S. Stevens <mailto:jon@latchkey.com>
 # Version $Id$
 # ------------------------------------------------------------------------- #
 
-if [ ! -f ./configure.in ]
+# ------------------------------------------------------------------------- #
+# Check if we are in the right directory to run this script                 # 
+# ------------------------------------------------------------------------- #
+if test ! -f ./configure.in
 then
-    echo "Cannot find \"configure.in\" file."
+    echo "cannot find \"configure.in\" file."
     exit 1
 fi
+
+# ------------------------------------------------------------------------- #
+# Check if we have the correct autoconf                                     # 
+# ------------------------------------------------------------------------- #
+echo "--- Checking \"autoconf\" version"
+VERSION=`autoconf --version 2> /dev/null | \
+    head -1 | \
+    sed -e 's/^[^0-9]*//' -e 's/[a-z]* *$//'`
+
+if test -z "${VERSION}"; then
+    echo "autoconf not found."
+    echo "autoconf version 2.13 or newer required to build from CVS."
+    exit 1
+fi
+
+IFS=.
+set $VERSION
+IFS=' '
+if test "$1" = "2" -a "$2" -lt "13" || test "$1" -lt "2"; then
+    echo "autoconf version $VERSION found."
+    echo "autoconf version 2.13 or newer required to build from CVS."
+    exit 1
+fi
+
+echo "autoconf version $VERSION detected."
+
+# ------------------------------------------------------------------------- #
+# Try to run the APR buildconf script if we have the sources locally        # 
+# ------------------------------------------------------------------------- #
+echo ""
 if [ -f "./apr/buildconf" ]
 then
-    echo "--- Running the \"buildconf\" script for APR"
-    ( cd ./apr
-    sh ./buildconf
+    echo "--- Running APR \"buildconf\" script"
+    (
+        cd ./apr
+        sh ./buildconf
     )
 else
     echo "--- Cannot run APR \"buildconf\" script"
-    echo "Don't a forget to download a copy of the APR sources, and to run"
-    echo "the buildconf script for it:"
+    echo "If you need to build the WebApp module for Apache 1.3"
+    echo "you will have to download the APR library sources from"
+    echo "http://apr.apache.org/ and run its \"buildconf\" script"
     echo "  # cd [path to APR sources]"
     echo "  # ./buildconf"
     echo "  # cd [path to WebApp sources]"
-    echo "Then remember to run ./configure script including the command line"
-    echo "option \"--with-apr=[path to APR sources]\""
+    echo "Then remember to run the WebApp \"configure\" script"
+    echo "specifying the \"--with-apr=[path to APR sources]\" extra"
+    echo "command line option."
 fi
+
+# ------------------------------------------------------------------------- #
+# Run autoconf to create the configure script                               # 
+# ------------------------------------------------------------------------- #
+echo ""
 echo "--- Creating WebApp \"configure\" script"
 echo "Creating configure ..."
 autoconf
+
+# ------------------------------------------------------------------------- #
+# Finish up                                                                 # 
+# ------------------------------------------------------------------------- #
+echo ""
 echo "--- All done"
