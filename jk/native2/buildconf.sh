5fcc0025e339df74fd257bc7d3779b8fbb8f7855
Gomez Henri <hgomez@apache.org>
2002-05-14 11:15:47 +0000

    Add reworked autoconf support
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@297194 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native2/buildconf.sh
+++ jk/native2/buildconf.sh
@@ -2,8 +2,8 @@
 
 echo "libtoolize --force --automake"
 libtoolize --force --automake
-echo "automake -a --foreign -i"
-automake -a --foreign -i
+#echo "automake -a --foreign -i"
+#automake -a --foreign -i
 echo "aclocal"
 #aclocal --acdir=`aclocal --print-ac-dir`
 #aclocal --acdir=/usr/local/share/aclocal
