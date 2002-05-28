63c8a79f813f36162b5ece1fc66e0a2b1a428e00
Jean-Frederic Clere <jfclere@apache.org>
2002-05-28 07:52:27 +0000

    automake needs the aclocal.m4 file so we have to run aclocal before automake
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@297372 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native2/buildconf.sh
+++ jk/native2/buildconf.sh
@@ -2,9 +2,9 @@
 
 echo "libtoolize --force --automake --copy"
 libtoolize --force --automake --copy
-echo "automake --copy --add-missing"
-automake --copy --add-missing
 echo "aclocal"
 aclocal
+echo "automake --copy --add-missing"
+automake --copy --add-missing
 echo "autoconf"
 autoconf
