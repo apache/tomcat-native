e5c8680054a54515b2557d4e3ea778a7220d1448
Jean-Frederic Clere <jfclere@apache.org>
2002-05-28 07:54:24 +0000

    put automake after aclocal.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@297373 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native/buildconf.sh
+++ jk/native/buildconf.sh
@@ -2,11 +2,11 @@
 
 echo "libtoolize --force --automake --copy"
 libtoolize --force --automake --copy
-echo "automake -a --foreign -i --copy"
-automake -a --foreign -i --copy
 echo "aclocal"
 #aclocal --acdir=`aclocal --print-ac-dir`
 #aclocal --acdir=/usr/local/share/aclocal
 aclocal
+echo "automake -a --foreign -i --copy"
+automake -a --foreign -i --copy
 echo "autoconf"
 autoconf
