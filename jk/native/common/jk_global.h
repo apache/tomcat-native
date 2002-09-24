a9fb3adcfe09266cf20cded6c16e8d89cfe5528d
Gomez Henri <hgomez@apache.org>
2002-09-24 22:34:08 +0000

    no socketvars.h on CYGWIN
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@297897 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native/common/jk_global.h
+++ jk/native/common/jk_global.h
@@ -98,7 +98,7 @@ extern char *strdup (const char *str);
         #include <netinet/tcp.h>
         #include <arpa/inet.h>
         #include <sys/un.h>
-        #if !defined(_OSD_POSIX) && !defined(AS400)
+        #if !defined(_OSD_POSIX) && !defined(AS400) && !defined(CYGWIN)
             #include <sys/socketvar.h>
         #endif
         #if !defined(HPUX11) && !defined(AS400)
