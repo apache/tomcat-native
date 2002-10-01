e6d9b5bf2f2162d6ec108f2ef3295fde2524f529
Costin Manolache <costin@apache.org>
2002-10-01 18:42:16 +0000

    Initial version - compile but probably doesn't run ( well, it did work at some stage ).
    This code is a refactored version of what's in catalina - I tried
    to merge different variants and standardise on use of Name.
    
    The reason of using Name - as I mentioned earlier - is the possible
    optimizations ( like using MessageBytes, caching, etc ), String is
    a very inflexible object.
    
    I added more comments to BaseDirContext on what I would like to
    do ( introspection-based config, etc ).
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@297957 13f79535-47bb-0310-9956-ffa450edef68

---
+++ naming/src/org/apache/naming/core/DirContextHelper.java
@@ -0,0 +1,108 @@
+/*
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 1999 The Apache Software Foundation.  All rights 
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:  
+ *       "This product includes software developed by the 
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Tomcat", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written 
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ * [Additional notices, if required by prior licensing conditions]
+ *
+ */ 
+
+
+package org.apache.naming.core;
+
+import java.util.Hashtable;
+import java.util.HashMap;
+import javax.naming.Context;
+import javax.naming.Name;
+import javax.naming.NameParser;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.CompositeName;
+import javax.naming.NameParser;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.ModificationItem;
+import javax.naming.directory.SearchControls;
+import org.apache.tomcat.util.res.StringManager;
+
+import java.io.*;
+
+/**
+ * Utility class providing additional operations on DirContexts.
+ * Instead of extending DirContext we use a helper who will work
+ * on any context, including 'foreign' ones ( like JNDI, etc ).
+ *
+ * Typical methods - conversions ( int, boolean), etc.
+ *
+ * This code should check if the context extend our BaseDirContext
+ * and use specific features ( like notes - to cache the conversion
+ * results for example ), but fallback for external contexts.
+ *
+ * @author Costin Manolache
+ */
+public class DirContextHelper  {
+    static DirContextHelper instance=new DirContextHelper();
+    
+    public static DirContextHelper getInstance() {
+        return instance;
+    }
+    
+    
+    /** Debugging string - the context, imediate childs and attributes
+     */
+    public String toString(DirContext ctx) {
+        return "";
+    }
+}
+
