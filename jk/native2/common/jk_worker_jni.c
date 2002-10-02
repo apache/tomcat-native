7574ec421646e9d71a7d759068202fe84ebfc127
Mladen Turk <mturk@apache.org>
2002-10-02 12:48:26 +0000

    We don't need the HAS_APR, cause we have HAVE_JNI.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@297972 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native2/common/jk_worker_jni.c
+++ jk/native2/common/jk_worker_jni.c
@@ -67,11 +67,9 @@
 #include "jk_env.h"
 #include "jk_bean.h"
 
-#ifdef HAS_APR
-#include "apr_thread_proc.h"
-#endif
-
 #ifdef HAVE_JNI
+
+#include "apr_thread_proc.h"
 
 #include "jk_vm.h"
 #include "jk_registry.h"
@@ -429,11 +427,9 @@ static int JK_METHOD jk2_jni_worker_destroy(jk_env_t *env, jk_bean_t *bean)
                                     jniWorker->jk_main_method,
                                     jargs);
         if (jniWorker->hook == JK2_WORKER_HOOK_SHUTDOWN) {
-#ifdef HAS_APR
             while (jk_jni_status_code != 2) {
                 apr_thread_yield();
             }
-#endif
             vm->detach(env, vm);
         }
     }
