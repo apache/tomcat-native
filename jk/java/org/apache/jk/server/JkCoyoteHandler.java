7054951558d940fd9dfd75f6618701e6d07d36cc
William Barker <billbarker@apache.org>
2002-11-10 07:25:21 +0000

    This time, without the tabs.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@298132 13f79535-47bb-0310-9956-ffa450edef68

--- jk/java/org/apache/jk/server/JkCoyoteHandler.java
+++ jk/java/org/apache/jk/server/JkCoyoteHandler.java
@@ -290,18 +290,18 @@ public class JkCoyoteHandler extends JkHandler implements
         
         MimeHeaders headers=res.getMimeHeaders();
         String contentType = res.getContentType();
-	if( contentType != null ) {
-	    headers.setValue("Content-Type").setString(contentType);
-	}
-	String contentLanguage = res.getContentLanguage();
-	if( contentLanguage != null ) {
-	    headers.setValue("Content-Language").setString(contentLanguage);
-	}
-	int contentLength = res.getContentLength();
-	if( contentLength >= 0 ) {
-	    headers.setValue("Content-Length").setInt(contentLength);
-	}
-	int numHeaders = headers.size();
+        if( contentType != null ) {
+            headers.setValue("Content-Type").setString(contentType);
+        }
+        String contentLanguage = res.getContentLanguage();
+        if( contentLanguage != null ) {
+            headers.setValue("Content-Language").setString(contentLanguage);
+        }
+        int contentLength = res.getContentLength();
+        if( contentLength >= 0 ) {
+            headers.setValue("Content-Length").setInt(contentLength);
+        }
+        int numHeaders = headers.size();
         msg.appendInt(numHeaders);
         for( int i=0; i<numHeaders; i++ ) {
             MessageBytes hN=headers.getName(i);
