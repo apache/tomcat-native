66efca0e522209509d2061bce504bd91a6f014c3
Mladen Turk <mturk@apache.org>
2002-11-27 17:12:05 +0000

    Change the version number to the 2.0.3 and mark as not nonreleased. The exposed version will be mod_jk2/2.0.3-beta-1. Perhaps we should change the beta to dev?
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@298196 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native2/include/jk_global.h
+++ jk/native2/include/jk_global.h
@@ -86,14 +86,14 @@ extern char *strdup (const char *str);
 /************** START OF AREA TO MODIFY BEFORE RELEASING *************/
 #define JK_VERMAJOR     2
 #define JK_VERMINOR     0
-#define JK_VERFIX       2
-#define JK_VERSTRING    "2.0.2"
+#define JK_VERFIX       3
+#define JK_VERSTRING    "2.0.3"
 
 /* Beta number */
 #define JK_VERBETA      1
 #define JK_BETASTRING   "1"
 /* set JK_VERISRELEASE to 1 when release (do not forget to commit!) */
-#define JK_VERISRELEASE 1
+#define JK_VERISRELEASE 0
 /************** END OF AREA TO MODIFY BEFORE RELEASING *************/
 
 #define PACKAGE "mod_jk2/"
