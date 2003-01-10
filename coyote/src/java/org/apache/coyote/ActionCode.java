205243162b7461361210b1c9abf546040142d182
Remy Maucherat <remm@apache.org>
2003-01-10 10:21:34 +0000

    - Fix code number.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@298298 13f79535-47bb-0310-9956-ffa450edef68

--- coyote/src/java/org/apache/coyote/ActionCode.java
+++ coyote/src/java/org/apache/coyote/ActionCode.java
@@ -134,7 +134,7 @@ public final class ActionCode {
      * Callback for lazy evaluation - extract the SSL-certificate 
      * (including forcing a re-handshake if necessary)
      */
-    public static final ActionCode ACTION_REQ_SSL_CERTIFICATE = new ActionCode(14);
+    public static final ActionCode ACTION_REQ_SSL_CERTIFICATE = new ActionCode(15);
 
 
     // ----------------------------------------------------------- Constructors
