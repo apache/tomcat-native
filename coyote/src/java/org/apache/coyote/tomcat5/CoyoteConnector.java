dcc714324a9ebae789ed4ede95728e75f26febd0
Remy Maucherat <remm@apache.org>
2003-01-14 21:50:41 +0000

    - Add an accessor for the protocol handler.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@298315 13f79535-47bb-0310-9956-ffa450edef68

--- coyote/src/java/org/apache/coyote/tomcat5/CoyoteConnector.java
+++ coyote/src/java/org/apache/coyote/tomcat5/CoyoteConnector.java
@@ -688,6 +688,16 @@ public final class CoyoteConnector
 
 
     /**
+     * Return the protocol handler associated with the connector.
+     */
+    public ProtocolHandler getProtocolHandler() {
+
+        return (this.protocolHandler);
+
+    }
+
+
+    /**
      * Return the proxy server name for this Connector.
      */
     public String getProxyName() {
