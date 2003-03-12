ba4294aba8ade3d230a9f649e8fdb300c7cb42f4
William Barker <billbarker@apache.org>
2003-03-12 07:41:04 +0000

    Handle the case of illegal values in the Cookie name/value pair.
    Reported by: Dennis Cartier <dennis@canadianblackbook.com>
    
    
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@298470 13f79535-47bb-0310-9956-ffa450edef68

--- coyote/src/java/org/apache/coyote/tomcat4/CoyoteAdapter.java
+++ coyote/src/java/org/apache/coyote/tomcat4/CoyoteAdapter.java
@@ -353,6 +353,7 @@ final class CoyoteAdapter
 
         Cookie[] cookies = new Cookie[count];
 
+	int idx=0;
         for (int i = 0; i < count; i++) {
             ServerCookie scookie = serverCookies.getCookie(i);
             if (scookie.getName().equals(Globals.SESSION_COOKIE_NAME)) {
@@ -369,9 +370,14 @@ final class CoyoteAdapter
                             .getRequestedSessionId());
                 }
             }
-            Cookie cookie = new Cookie(scookie.getName().toString(),
+	    try {
+		Cookie cookie = new Cookie(scookie.getName().toString(),
                                        scookie.getValue().toString());
-            cookies[i] = cookie;
+		cookies[idx++] = cookie;
+	    } catch (Exception ex) {
+		log("Bad Cookie Name: " + scookie.getName() + 
+		    " /Value: " + scookie.getValue(),ex);
+	    }
         }
 
         request.setCookies(cookies);
