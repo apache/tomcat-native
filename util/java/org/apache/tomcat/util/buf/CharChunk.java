02ef8ce0900572c96fbf2170e9330eb5ac4f8019
Remy Maucherat <remm@apache.org>
2003-05-01 20:50:50 +0000

    - I think I forgot something (Costin style) ...
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@298644 13f79535-47bb-0310-9956-ffa450edef68

--- util/java/org/apache/tomcat/util/buf/CharChunk.java
+++ util/java/org/apache/tomcat/util/buf/CharChunk.java
@@ -690,6 +690,25 @@ public final class CharChunk implements Cloneable, Serializable {
     }
     
 
+    public int indexOf( String src, int srcOff, int srcLen, int myOff ) {
+	char first=src.charAt( srcOff );
+
+	// Look for first char 
+	int srcEnd = srcOff + srcLen;
+        
+	for( int i=myOff+start; i <= (end - srcLen); i++ ) {
+	    if( buff[i] != first ) continue;
+	    // found first char, now look for a match
+            int myPos=i+1;
+	    for( int srcPos=srcOff + 1; srcPos< srcEnd; ) {
+                if( buff[myPos++] != src.charAt( srcPos++ ))
+		    break;
+                if( srcPos==srcEnd ) return i-start; // found it
+	    }
+	}
+	return -1;
+    }
+
     // -------------------- utils
     private int min(int a, int b) {
 	if (a < b) return a;
