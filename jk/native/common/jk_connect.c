d1d3c78abf4c3c93a14f5a33638a3aa330e0660c
Gomez Henri <hgomez@apache.org>
2004-07-14 08:37:05 +0000

    For now the sa_len is set only when using iSeries in Unix 98 mode...
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@299490 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native/common/jk_connect.c
+++ jk/native/common/jk_connect.c
@@ -145,7 +145,7 @@ int jk_open_socket(struct sockaddr_in *addr,
                    
 /* Need more infos for BSD 4.4 and Unix 98 defines, for now only 
    iSeries when Unix98 is required at compil time */
-#if (_XOPEN_SOURCE >= 520)
+#if (_XOPEN_SOURCE >= 520) && defined(AS400)
 	       ((struct sockaddr *)addr)->sa_len = sizeof(struct sockaddr_in));
 #endif 
             ret = connect(sock,
