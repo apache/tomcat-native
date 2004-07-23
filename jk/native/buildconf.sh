d058810fc8bebbf96ec7efe209a4dcd5bc2a3a56
Jean-Frederic Clere <jfclere@apache.org>
2004-07-23 08:06:30 +0000

    Remove the automake cache directory.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@299514 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native/buildconf.sh
+++ jk/native/buildconf.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "rm autom4te.cache"
+rm -rf autom4te.cache
+
 echo "libtoolize --force --automake --copy"
 libtoolize --force --automake --copy
 echo "aclocal"
@@ -12,3 +15,6 @@ echo "automake -a --foreign --copy"
 automake -a --foreign --copy
 echo "autoconf"
 autoconf
+
+echo "rm autom4te.cache"
+rm -rf autom4te.cache
