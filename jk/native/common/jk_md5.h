c587058c0d36349a5de890e1ef296702638cd5ad
Mladen Turk <mturk@apache.org>
2004-11-08 13:31:55 +0000

    Use platform 32 bit integers
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@299853 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native/common/jk_md5.h
+++ jk/native/common/jk_md5.h
@@ -57,23 +57,19 @@ extern "C"
 #define JK_MD5_DIGESTSIZE 16
 
 /* JK_UINT4 defines a four byte word */
-#if HAVE_APR
-    typedef apr_uint32_t JK_UINT4;
-#else
 #ifdef WIN32
     typedef DWORD JK_UINT4;
 #else
-    typedef unsigned int JK_UINT4;
+    typedef uint32_t JK_UINT4;
 #endif
-#endif                          /* HAVE_APR */
 
 /* MD5 context. */
-    typedef struct
-    {
-        JK_UINT4 state[4];      /* state (ABCD) */
-        JK_UINT4 count[2];      /* number of bits, modulo 2^64 (lsb first) */
-        unsigned char buffer[64];       /* input buffer */
-    } JK_MD5_CTX;
+typedef struct
+{
+    JK_UINT4 state[4];      /* state (ABCD) */
+    JK_UINT4 count[2];      /* number of bits, modulo 2^64 (lsb first) */
+    unsigned char buffer[64];       /* input buffer */
+} JK_MD5_CTX;
 
 /*
  * Define the Magic String prefix that identifies a password as being
@@ -82,9 +78,9 @@ extern "C"
 #define JK_MD5PW_ID "$apr1$"
 #define JK_MD5PW_IDLEN 6
 
-    char *JK_METHOD jk_hextocstr(unsigned char *org, char *dst, int n);
-    char *JK_METHOD jk_md5(const unsigned char *org,
-                           const unsigned char *org2, char *dst);
+char *JK_METHOD jk_hextocstr(unsigned char *org, char *dst, int n);
+char *JK_METHOD jk_md5(const unsigned char *org,
+                       const unsigned char *org2, char *dst);
 
 
 #ifdef __cplusplus
