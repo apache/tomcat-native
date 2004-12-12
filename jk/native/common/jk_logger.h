3a6603b5fb769898cbc67457f949508851087a76
Mladen Turk <mturk@apache.org>
2004-12-12 15:34:08 +0000

    Fix typo.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@300014 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native/common/jk_logger.h
+++ jk/native/common/jk_logger.h
@@ -59,7 +59,7 @@ struct file_logger_t
 #define JK_LOG_EMERG_LEVEL   5
 #define JK_LOG_REQUEST_LEVEL 6
 
-#define JK_LOG_TRACE_WERB   "trace"
+#define JK_LOG_TRACE_VERB   "trace"
 #define JK_LOG_DEBUG_VERB   "debug"
 #define JK_LOG_INFO_VERB    "info"
 #define JK_LOG_WARNING_VERB "warn"
