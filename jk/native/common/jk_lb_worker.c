643c4a0d5038072056a79eeeff9a7487eff2657e
Mladen Turk <mturk@apache.org>
2005-02-13 12:21:57 +0000

    Use shm_alloc_worker, so that worker get it's id set, and that we can monitor the total number of workers.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@300188 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native/common/jk_lb_worker.c
+++ jk/native/common/jk_lb_worker.c
@@ -580,7 +580,7 @@ static int JK_METHOD validate(jk_worker_t *pThis,
             }
 
             for (i = 0; i < num_of_workers; i++) {
-                p->lb_workers[i].s = jk_shm_alloc(&p->p, sizeof(jk_shm_worker_t));
+                p->lb_workers[i].s = jk_shm_alloc_worker(&p->p);
                 if (p->lb_workers[i].s == NULL) {
                     jk_log(l, JK_LOG_ERROR, 
                            "allocating worker record from shared memory");
@@ -778,7 +778,7 @@ int JK_METHOD lb_worker_factory(jk_worker_t **w,
                         private_data->buf,
                         sizeof(jk_pool_atom_t) * TINY_POOL_SIZE);
 
-        private_data->s = jk_shm_alloc(&private_data->p, sizeof(jk_shm_worker_t));
+        private_data->s = jk_shm_alloc_worker(&private_data->p);
         if (!private_data->s) {
             free(private_data);
             JK_TRACE_EXIT(l);
