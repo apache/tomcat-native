ac04d28f407cf338f43a96c62fb32fbb4962d924
Mladen Turk <mturk@apache.org>
2005-02-21 09:34:25 +0000

    Use new load properties api.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@300296 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native/netscape/jk_nsapi_plugin.c
+++ jk/native/netscape/jk_nsapi_plugin.c
@@ -235,7 +235,7 @@ NSAPI_PUBLIC int jk_init(pblock * pb, Session * sn, Request * rq)
     
     jk_shm_open(shm_file, JK_SHM_DEF_SIZE, logger);
     if (jk_map_alloc(&init_map)) {
-        if (jk_map_read_properties(init_map, worker_prp_file)) {
+        if (jk_map_read_properties(init_map, worker_prp_file, NULL)) {
             int sleep_cnt;
             SYS_THREAD s;
 
