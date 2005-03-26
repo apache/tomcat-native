1679261a6fdc87f1166771e2d45582600d6858da
Mladen Turk <mturk@apache.org>
2005-03-26 12:24:01 +0000

    Add shell script for making jk releases.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@300386 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jk/tools/jkrelease.sh
@@ -0,0 +1,34 @@
+#/bin/sh
+
+# You can change JKTAG and JKVER to desired CVS tag.
+JKTAG="HEAD"
+JKVER="-current-src"
+export CVSROOT=:pserver:anoncvs@cvs.apache.org:/home/cvspublic
+cvs login
+cvs export -r ${JKTAG} -d jakarta-tomcat-connectors${JKVER} jakarta-tomcat-connectors
+# Remove all files that are not part of jk release
+rm -rf jakarta-tomcat-connectors${JKVER}/ajp
+rm -rf jakarta-tomcat-connectors${JKVER}/coyote
+rm -rf jakarta-tomcat-connectors${JKVER}/http11
+rm -rf jakarta-tomcat-connectors${JKVER}/jk/java
+rm -rf jakarta-tomcat-connectors${JKVER}/jk/jkant
+rm -rf jakarta-tomcat-connectors${JKVER}/jk/native2
+rm -rf jakarta-tomcat-connectors${JKVER}/jk/test
+rm -rf jakarta-tomcat-connectors${JKVER}/jni
+rm -rf jakarta-tomcat-connectors${JKVER}/juli
+rm -rf jakarta-tomcat-connectors${JKVER}/naming
+rm -rf jakarta-tomcat-connectors${JKVER}/procrun
+rm -rf jakarta-tomcat-connectors${JKVER}/util
+rm -rf jakarta-tomcat-connectors${JKVER}/webapp
+
+# Build documentation.
+cd jakarta-tomcat-connectors${JKVER}/jk/xdocs
+ant
+cd ../native
+./buildconf.sh
+cd ../../../
+tar cvf jakarta-tomcat-connectors${JKVER}.tar jakarta-tomcat-connectors${JKVER}
+gzip jakarta-tomcat-connectors${JKVER}.tar
+
+# Create detatched signature
+gpg -ba jakarta-tomcat-connectors${JKVER}.tar.gz
