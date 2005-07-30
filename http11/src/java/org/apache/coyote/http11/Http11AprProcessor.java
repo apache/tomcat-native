57f88bf80c5d84f9e69450e7b7ee1552186c4200
Mark Emlyn David Thomas <markt@apache.org>
2005-07-30 16:18:14 +0000

    Fix typo. Should also fix Gump.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@301008 13f79535-47bb-0310-9956-ffa450edef68

--- http11/src/java/org/apache/coyote/http11/Http11AprProcessor.java
+++ http11/src/java/org/apache/coyote/http11/Http11AprProcessor.java
@@ -34,7 +34,7 @@ import org.apache.coyote.Adapter;
 import org.apache.coyote.Request;
 import org.apache.coyote.RequestInfo;
 import org.apache.coyote.Response;
-import org.apache.coyote.ajp.Constants;
+import org.apache.coyote.http11.Constants;
 import org.apache.coyote.http11.filters.ChunkedInputFilter;
 import org.apache.coyote.http11.filters.ChunkedOutputFilter;
 import org.apache.coyote.http11.filters.GzipOutputFilter;
