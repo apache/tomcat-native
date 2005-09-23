97961df4dd68413ed8adab47406b0d5c5d91f626
Mladen Turk <mturk@apache.org>
2005-09-23 11:44:44 +0000

    Bump the release version
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@301068 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native/common/jk_version.h
+++ jk/native/common/jk_version.h
@@ -32,7 +32,7 @@
 #define JK_VERBETA      0
 #define JK_BETASTRING   "0"
 /* set JK_VERISRELEASE to 1 when release (do not forget to commit!) */
-#define JK_VERISRELEASE 0
+#define JK_VERISRELEASE 1
 #define JK_VERRC        0
 #define JK_RCSTRING     "0"
 
