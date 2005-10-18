a65efba4b4377ee491a125c383b3baccbdd84f90
Yoav Shapira <yoavs@apache.org>
2005-10-18 17:03:28 +0000

    Typo fix in error message, pointed out to me via private email by Entiaz Inan.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@326153 13f79535-47bb-0310-9956-ffa450edef68

--- util/java/org/apache/tomcat/util/threads/ThreadPool.java
+++ util/java/org/apache/tomcat/util/threads/ThreadPool.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 1999-2004 The Apache Software Foundation
+ *  Copyright 1999-2005 The Apache Software Foundation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -172,7 +172,7 @@ public class ThreadPool  {
       if (threadPriority < Thread.MIN_PRIORITY) {
         throw new IllegalArgumentException("new priority < MIN_PRIORITY");
       } else if (threadPriority > Thread.MAX_PRIORITY) {
-        throw new IllegalArgumentException("new priority > MIN_PRIORITY");
+        throw new IllegalArgumentException("new priority > MAX_PRIORITY");
       }
 
       // Set for future threads
