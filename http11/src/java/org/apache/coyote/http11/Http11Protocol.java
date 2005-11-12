607024363be05c6bb5b68b0bbd7a156c75469c98
Peter Rossbach <pero@apache.org>
2005-11-12 19:28:55 +0000

    Remove useless init code
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@332825 13f79535-47bb-0310-9956-ffa450edef68

--- http11/src/java/org/apache/coyote/http11/Http11Protocol.java
+++ http11/src/java/org/apache/coyote/http11/Http11Protocol.java
@@ -59,13 +59,6 @@ import org.apache.tomcat.util.threads.ThreadWithAttributes;
 public class Http11Protocol extends Http11BaseProtocol implements MBeanRegistration
 {
     public Http11Protocol() {
-        /*
-        cHandler = new JmxHttp11ConnectionHandler( this );
-        setSoLinger(Constants.DEFAULT_CONNECTION_LINGER);
-        setSoTimeout(Constants.DEFAULT_CONNECTION_TIMEOUT);
-        setServerSoTimeout(Constants.DEFAULT_SERVER_SOCKET_TIMEOUT);
-        setTcpNoDelay(Constants.DEFAULT_TCP_NO_DELAY);
-        */
     }
     
     protected Http11ConnectionHandler createConnectionHandler() {
