37570c6f1299c47c9274a727aee5584409ebe793
Mladen Turk <mturk@apache.org>
2006-07-08 06:52:09 +0000

    Increment the version to 1.2.17
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@420063 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native/common/jk_version.h
+++ jk/native/common/jk_version.h
@@ -25,8 +25,8 @@
 /************** START OF AREA TO MODIFY BEFORE RELEASING *************/
 #define JK_VERMAJOR     1
 #define JK_VERMINOR     2
-#define JK_VERFIX       16
-#define JK_VERSTRING    "1.2.16"
+#define JK_VERFIX       17
+#define JK_VERSTRING    "1.2.17"
 
 /* Beta number */
 #define JK_VERBETA      0
