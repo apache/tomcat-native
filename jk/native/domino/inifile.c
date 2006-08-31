7af5c4de89b55e50a3496ca060b94780c8b32ccb
Mladen Turk <mturk@apache.org>
2006-08-31 13:06:45 +0000

    Revert recent remove.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@438903 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jk/native/domino/inifile.c
@@ -0,0 +1,277 @@
+/*
+ *  Copyright 1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/***************************************************************************
+ * Description: DSAPI plugin for Lotus Domino                              *
+ * Author:      Andy Armstrong <andy@tagish.com>                           *
+ * Date:        20010603                                                   *
+ * Version:     $Revision$                                           *
+ ***************************************************************************/
+
+#include "config.h"
+#include "inifile.h"
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/* We have one of these for each ini file line. Once we've read the
+ * file and parsed it we'll have an array in key order containing one
+ * of these for each configuration item in file.
+ */
+typedef struct
+{
+    const char *key;
+    const char *value;
+
+} inifile_key;
+
+static char *file;              /* the text of the ini file                             */
+static inifile_key *keys;       /* an array of keys, one per item               */
+static size_t klen;             /* length of the key array                              */
+
+/* Text that will prefix all of our error messages */
+#define ERRPFX "INIFILE: "
+/* Various error messages that we can return */
+ERRTYPE inifile_outofmemory = ERRPFX "Out of memory";
+ERRTYPE inifile_filenotfound = ERRPFX "File not found";
+ERRTYPE inifile_readerror = ERRPFX "Error reading file";
+#define SYNFMT ERRPFX "File %s, line %d: %s"
+
+/* Case insensitive string comparison, works like strcmp() */
+static int inifile__stricmp(const char *s1, const char *s2)
+{
+    while (*s1 && tolower(*s1) == tolower(*s2))
+        s1++, s2++;
+    return tolower(*s1) - tolower(*s2);
+}
+
+/* Compare keys, suitable for passing to qsort() */
+static int inifile__cmp(const void *k1, const void *k2)
+{
+    const inifile_key *kk1 = (const inifile_key *)k1;
+    const inifile_key *kk2 = (const inifile_key *)k2;
+    return inifile__stricmp(kk1->key, kk2->key);
+}
+
+/* Return a new syntax error message. */
+static ERRTYPE inifile__syntax(jk_pool_t *p, const char *file, int line,
+                               const char *msg)
+{
+    static const char synfmt[] = SYNFMT;
+    size_t len =
+        sizeof(synfmt) + strlen(msg) + strlen(file) +
+        10 /* fudge for line number */ ;
+    char *buf = jk_pool_alloc(p, len);
+    sprintf(buf, synfmt, file, line, msg);
+    return buf;
+}
+
+/* Various macros to tidy up the parsing code */
+
+/* Characters that are OK in the keyname */
+#define KEYCHR(c) \
+	(isalnum(c) || (c) == '.' || (c) == '_')
+
+/* Skip whitespace */
+#define SKIPSPC() \
+	while (*fp == '\t' || *fp == ' ') fp++
+
+/* Skip to the end of the current line */
+#define SKIPLN() \
+	while (*fp != '\0' && *fp != '\r' && *fp != '\n') fp++
+
+/* Move from the end of the current line to the start of the next, learning what the
+ * newline character is and counting lines
+ */
+#define NEXTLN() \
+	do { while (*fp == '\r' || *fp == '\n') { if (nlc == -1) nlc = *fp; if (*fp == nlc) ln++; fp++; } } while (0)
+
+/* Build the index. Called when the inifile is loaded by inifile_load()
+ */
+static ERRTYPE inifile__index(jk_pool_t *p, const char *name)
+{
+    int pass;
+    int ln = 1;
+    int nlc = -1;
+
+    /* Make two passes over the data. First time we're just counting
+     * the lines that contain values so we can allocate the index, second
+     * time we build the index.
+     */
+    for (pass = 0; pass < 2; pass++) {
+        char *fp = file;
+        char *ks = NULL, *ke;   /* key start, end */
+        char *vs = NULL, *ve;   /* value start, end */
+        klen = 0;
+
+        while (*fp != '\0') {
+            SKIPSPC();
+
+            /* turn a comment into an empty line by moving to the next \r|\n */
+            if (*fp == '#' || *fp == ';')
+                SKIPLN();
+
+            if (*fp != '\0' && *fp != '\r' && *fp != '\n') {
+                ks = fp;        /* start of key */
+                while (KEYCHR(*fp))
+                    fp++;
+                ke = fp;        /* end of key */
+                SKIPSPC();
+
+                if (*fp != '=')
+                    return inifile__syntax(p, name, ln,
+                                           "Missing '=' or illegal character in key");
+
+                fp++;           /* past the = */
+                SKIPSPC();
+                vs = fp;
+                SKIPLN();
+                ve = fp;
+                /* back up over any trailing space */
+                while (ve > vs && (ve[-1] == ' ' || ve[-1] == '\t'))
+                    ve--;
+                NEXTLN();       /* move forwards *before* we trash the eol characters */
+
+                if (NULL != keys) {     /* second pass? if so stash a pointer */
+                    *ke = '\0';
+                    *ve = '\0';
+                    keys[klen].key = ks;
+                    keys[klen].value = vs;
+                }
+
+                klen++;
+            }
+            else {
+                NEXTLN();
+            }
+        }
+
+        if (NULL == keys
+            && (keys =
+                jk_pool_alloc(p, sizeof(inifile_key) * klen), NULL == keys))
+            return inifile_outofmemory;
+    }
+
+    /* got the index now, sort it so we can search it quickly */
+    qsort(keys, klen, sizeof(inifile_key), inifile__cmp);
+
+    return ERRNONE;
+}
+
+/* Read an INI file from disk
+ */
+ERRTYPE inifile_read(jk_pool_t *p, const char *name)
+{
+    FILE *fl;
+    size_t flen;
+    int ok;
+
+    if (fl = fopen(name, "rb"), NULL == fl)
+        return inifile_filenotfound;
+
+    fseek(fl, 0L, SEEK_END);
+    flen = (size_t) ftell(fl);
+    fseek(fl, 0L, SEEK_SET);
+
+    /* allocate one extra byte for trailing \0
+     */
+    if (file = jk_pool_alloc(p, flen + 1), NULL == file) {
+        fclose(fl);
+        return inifile_outofmemory;
+    }
+
+    ok = (fread(file, flen, 1, fl) == 1);
+    fclose(fl);
+    if (!ok)
+        return inifile_readerror;
+
+    file[flen] = '\0';          /* terminate it to simplify parsing */
+
+    return inifile__index(p, name);
+}
+
+/* Find the value associated with the given key returning it or NULL
+ * if no match is found. Key name matching is case insensitive.
+ */
+const char *inifile_lookup(const char *key)
+{
+    int lo, mid, hi, cmp;
+
+    if (NULL == keys)
+        return NULL;
+
+    for (lo = 0, hi = klen - 1; lo <= hi;) {
+        mid = (lo + hi) / 2;
+        cmp = inifile__stricmp(key, keys[mid].key);
+        if (cmp < 0)            /* key in array is greater */
+            hi = mid - 1;
+        else if (cmp > 0)
+            lo = mid + 1;
+        else
+            return keys[mid].value;
+    }
+
+    return NULL;
+}
+
+#ifdef TEST
+
+static jk_pool_t pool;
+extern void jk_dump_pool(jk_pool_t *p, FILE * f);       /* not declared in header */
+
+int main(void)
+{
+    ERRTYPE e;
+    unsigned k;
+    int rc = 0;
+
+    jk_open_pool(&pool, NULL, 0);
+
+    e = inifile_read(&pool, "ok.ini");
+    if (e == ERRNONE) {
+        printf("%u keys in ok.ini\n", klen);
+        for (k = 0; k < klen; k++) {
+            const char *val = inifile_lookup(keys[k].key);
+            printf("Key: \"%s\", value: \"%s\"\n", keys[k].key, val);
+        }
+    }
+    else {
+        printf("Error reading ok.ini: %s\n", e);
+        rc = 1;
+    }
+
+    e = inifile_read(&pool, "bad.ini");
+    if (e == ERRNONE) {
+        printf("%u keys in bad.ini\n", klen);
+        for (k = 0; k < klen; k++) {
+            const char *val = inifile_lookup(keys[k].key);
+            printf("Key: \"%s\", value: \"%s\"\n", keys[k].key, val);
+        }
+        rc = 1;                 /* should be a syntax error */
+    }
+    else {
+        printf
+            ("Error reading bad.ini: %s (which is OK: that's what we expected)\n",
+             e);
+    }
+
+    jk_dump_pool(&pool, stdout);
+    jk_close_pool(&pool);
+
+    return rc;
+}
+#endif
---
+++ jk/native/iis/isapi.dsp
@@ -0,0 +1,299 @@
+# Microsoft Developer Studio Project File - Name="isapi" - Package Owner=<4>
+# Microsoft Developer Studio Generated Build File, Format Version 6.00
+# ** DO NOT EDIT **
+
+# TARGTYPE "Win32 (x86) Dynamic-Link Library" 0x0102
+
+CFG=isapi - Win32 Debug
+!MESSAGE This is not a valid makefile. To build this project using NMAKE,
+!MESSAGE use the Export Makefile command and run
+!MESSAGE 
+!MESSAGE NMAKE /f "isapi.mak".
+!MESSAGE 
+!MESSAGE You can specify a configuration when running NMAKE
+!MESSAGE by defining the macro CFG on the command line. For example:
+!MESSAGE 
+!MESSAGE NMAKE /f "isapi.mak" CFG="isapi - Win32 Debug"
+!MESSAGE 
+!MESSAGE Possible choices for configuration are:
+!MESSAGE 
+!MESSAGE "isapi - Win32 Release" (based on "Win32 (x86) Dynamic-Link Library")
+!MESSAGE "isapi - Win32 Debug" (based on "Win32 (x86) Dynamic-Link Library")
+!MESSAGE 
+
+# Begin Project
+# PROP AllowPerConfigDependencies 0
+# PROP Scc_ProjName ""
+# PROP Scc_LocalPath ""
+CPP=cl.exe
+MTL=midl.exe
+RSC=rc.exe
+
+!IF  "$(CFG)" == "isapi - Win32 Release"
+
+# PROP BASE Use_MFC 0
+# PROP BASE Use_Debug_Libraries 0
+# PROP BASE Output_Dir "Release"
+# PROP BASE Intermediate_Dir "Release"
+# PROP BASE Target_Dir ""
+# PROP Use_MFC 0
+# PROP Use_Debug_Libraries 0
+# PROP Output_Dir "Release"
+# PROP Intermediate_Dir "Release"
+# PROP Ignore_Export_Lib 0
+# PROP Target_Dir ""
+# ADD BASE CPP /nologo /MT /W3 /GX /O2 /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "ISAPI_EXPORTS" /YX /FD /c
+# ADD CPP /nologo /MD /W3 /Zi /O2 /I "..\common" /I "$(JAVA_HOME)\include" /I "$(JAVA_HOME)\include\win32" /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "ISAPI_EXPORTS" /Fd"Release/isapi_redirector_src" /FD /c
+# ADD BASE MTL /nologo /D "NDEBUG" /mktyplib203 /win32
+# ADD MTL /nologo /D "NDEBUG" /mktyplib203 /win32
+# ADD BASE RSC /l 0x809 /d "NDEBUG"
+# ADD RSC /l 0x809 /d "NDEBUG"
+BSC32=bscmake.exe
+# ADD BASE BSC32 /nologo
+# ADD BSC32 /nologo
+LINK32=link.exe
+# ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /dll /machine:I386
+# ADD LINK32 kernel32.lib user32.lib advapi32.lib ws2_32.lib mswsock.lib /nologo /base:"0x6A6B0000" /dll /debug /machine:I386 /out:"Release\isapi_redirect.dll"
+
+!ELSEIF  "$(CFG)" == "isapi - Win32 Debug"
+
+# PROP BASE Use_MFC 0
+# PROP BASE Use_Debug_Libraries 1
+# PROP BASE Output_Dir "Debug"
+# PROP BASE Intermediate_Dir "Debug"
+# PROP BASE Target_Dir ""
+# PROP Use_MFC 0
+# PROP Use_Debug_Libraries 1
+# PROP Output_Dir "Debug"
+# PROP Intermediate_Dir "Debug"
+# PROP Ignore_Export_Lib 0
+# PROP Target_Dir ""
+# ADD BASE CPP /nologo /MTd /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "ISAPI_EXPORTS" /YX /FD /GZ /c
+# ADD CPP /nologo /MDd /W3 /GX /Zi /Od /I "..\common" /I "$(JAVA_HOME)\include" /I "$(JAVA_HOME)\include\win32" /D "_DEBUG" /D "WIN32" /D "_WINDOWS" /Fd"Debug/isapi_redirector_src" /FD /c
+# ADD BASE MTL /nologo /D "_DEBUG" /mktyplib203 /win32
+# ADD MTL /nologo /D "_DEBUG" /mktyplib203 /win32
+# ADD BASE RSC /l 0x809 /d "_DEBUG"
+# ADD RSC /l 0x809 /d "_DEBUG"
+BSC32=bscmake.exe
+# ADD BASE BSC32 /nologo
+# ADD BSC32 /nologo
+LINK32=link.exe
+# ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /dll /debug /machine:I386 /pdbtype:sept
+# ADD LINK32 kernel32.lib user32.lib advapi32.lib ws2_32.lib mswsock.lib /nologo /base:"0x6A6B0000" /dll /incremental:no /debug /machine:I386 /out:"Debug\isapi_redirect.dll"
+
+!ENDIF 
+
+# Begin Target
+
+# Name "isapi - Win32 Release"
+# Name "isapi - Win32 Debug"
+# Begin Group "Source Files"
+
+# PROP Default_Filter "cpp;c;cxx;rc;def;r;odl;idl;hpj;bat"
+# Begin Source File
+
+SOURCE=.\isapi_redirect.rc
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_ajp12_worker.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_ajp13.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_ajp13_worker.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_ajp14.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_ajp14_worker.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_ajp_common.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_connect.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_context.c
+# End Source File
+# Begin Source File
+
+SOURCE=.\jk_isapi_plugin.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_jni_worker.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_lb_worker.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_map.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_md5.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_msg_buff.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_nwmain.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_pool.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_shm.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_sockbuf.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_status.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_uri_worker_map.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_util.c
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_worker.c
+# End Source File
+# End Group
+# Begin Group "Header Files"
+
+# PROP Default_Filter "h;hpp;hxx;hm;inl"
+# Begin Source File
+
+SOURCE=..\common\jk_ajp13.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_ajp13_worker.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_ajp14.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_ajp14_worker.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_ajp_common.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_channel.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_connect.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_context.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_env.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_global.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_jni_worker.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_lb_worker.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_logger.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_map.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_md5.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_msg_buff.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_mt.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_pool.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_service.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_shm.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_status.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_uri_worker_map.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_util.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_version.h
+# End Source File
+# Begin Source File
+
+SOURCE=..\common\jk_worker.h
+# End Source File
+# End Group
+# Begin Group "Resource Files"
+
+# PROP Default_Filter "ico;cur;bmp;dlg;rc2;rct;bin;rgs;gif;jpg;jpeg;jpe"
+# Begin Source File
+
+SOURCE=.\isapi.def
+# End Source File
+# End Group
+# End Target
+# End Project
