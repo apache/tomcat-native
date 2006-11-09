7a607ca922312c5ce26e5eef39870a67526ca8e4
Mark Emlyn David Thomas <markt@apache.org>
2006-11-09 03:05:09 +0000

    Fix bug 37509. My reading of the Properties JavaDoc is that property values can't start with white space. Given that values can't start with white space and having white space on the end is valid then I can't see what the purpose of this trim() is.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@472751 13f79535-47bb-0310-9956-ffa450edef68

--- juli/src/java/org/apache/juli/ClassLoaderLogManager.java
+++ juli/src/java/org/apache/juli/ClassLoaderLogManager.java
@@ -452,7 +452,7 @@ public class ClassLoaderLogManager extends LogManager {
      * @return the modified string
      */
     protected String replace(String str) {
-        String result = str.trim();
+        String result = str;
         if (result.startsWith("${")) {
             int pos = result.indexOf('}');
             if (pos != -1) {
