c4f5d6c58462338f2ccf7f8eeee1d9a283c06e52
Rainer Jung <rjung@apache.org>
2007-04-09 19:45:43 +0000

    No functional change - make comments less misleading.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@526888 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native/common/jk_lb_worker.c
+++ jk/native/common/jk_lb_worker.c
@@ -832,34 +832,34 @@ static void lb_add_log_items(jk_ws_service_t *s,
     const char **log_values = jk_pool_alloc(s->pool, sizeof(char *) * JK_LB_NOTES_COUNT);
     char *buf = jk_pool_alloc(s->pool, sizeof(char *) * JK_LB_NOTES_COUNT * JK_LB_UINT64_STR_SZ);
     if (log_values && buf) {
-        /* JK_NOTE_LB_FIRST_NAME */
+        /* JK_NOTE_LB_FIRST/LAST_NAME */
         log_values[0] = w->s->name;
         snprintf(buf, JK_LB_UINT64_STR_SZ, "%" JK_UINT64_T_FMT, w->s->lb_value);
-        /* JK_NOTE_LB_FIRST_VALUE */
+        /* JK_NOTE_LB_FIRST/LAST_VALUE */
         log_values[1] = buf;
         buf += JK_LB_UINT64_STR_SZ;
         snprintf(buf, JK_LB_UINT64_STR_SZ, "%" JK_UINT64_T_FMT, w->s->elected);
-        /* JK_NOTE_LB_FIRST_ACCESSED */
+        /* JK_NOTE_LB_FIRST/LAST_ACCESSED */
         log_values[2] = buf;
         buf += JK_LB_UINT64_STR_SZ;
         snprintf(buf, JK_LB_UINT64_STR_SZ, "%" JK_UINT64_T_FMT, w->s->readed);
-        /* JK_NOTE_LB_FIRST_READ */
+        /* JK_NOTE_LB_FIRST/LAST_READ */
         log_values[3] = buf;
         buf += JK_LB_UINT64_STR_SZ;
         snprintf(buf, JK_LB_UINT64_STR_SZ, "%" JK_UINT64_T_FMT, w->s->transferred);
-        /* JK_NOTE_LB_FIRST_TRANSFERRED */
+        /* JK_NOTE_LB_FIRST/LAST_TRANSFERRED */
         log_values[4] = buf;
         buf += JK_LB_UINT64_STR_SZ;
         snprintf(buf, JK_LB_UINT64_STR_SZ, "%" JK_UINT32_T_FMT, w->s->errors);
-        /* JK_NOTE_LB_FIRST_ERRORS */
+        /* JK_NOTE_LB_FIRST/LAST_ERRORS */
         log_values[5] = buf;
         buf += JK_LB_UINT64_STR_SZ;
         snprintf(buf, JK_LB_UINT64_STR_SZ, "%d", w->s->busy);
-        /* JK_NOTE_LB_FIRST_BUSY */
+        /* JK_NOTE_LB_FIRST/LAST_BUSY */
         log_values[6] = buf;
-        /* JK_NOTE_LB_FIRST_ACTIVATION */
+        /* JK_NOTE_LB_FIRST/LAST_ACTIVATION */
         log_values[7] = jk_lb_get_activation(w, l);
-        /* JK_NOTE_LB_FIRST_STATE */
+        /* JK_NOTE_LB_FIRST/LAST_STATE */
         log_values[8] = jk_lb_get_state(w, l);
         s->add_log_items(s, log_names, log_values, JK_LB_NOTES_COUNT);
     }
