2c4758778c6bc005b82509363b6b87470bf8aceb
Rainer Jung <rjung@apache.org>
2007-06-25 16:04:31 +0000

    Fix comparison between signed and unsigned type. Uncritical here, because value of this config parameter won't be negative or very large.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@550530 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native/common/jk_lb_worker.c
+++ jk/native/common/jk_lb_worker.c
@@ -1094,7 +1094,7 @@ static int JK_METHOD service(jk_endpoint_t *e,
                         rec->s->reply_timeouts++;
                     }
                     if (service_stat != JK_REPLY_TIMEOUT ||
-                        rec->s->reply_timeouts > p->worker->s->max_reply_timeouts) {
+                        rec->s->reply_timeouts > (unsigned)p->worker->s->max_reply_timeouts) {
 
                         /*
                         * Service failed !!!
