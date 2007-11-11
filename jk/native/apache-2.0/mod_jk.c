e69f54892a08e9aed8eb5e8a177236024224ab84
Rainer Jung <rjung@apache.org>
2007-11-11 16:06:27 +0000

    We should always have a non-NULL mapping table in the main server. Otherwise e.g. we get a core dump, when using the mount attribute in workers.properties.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@593918 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native/apache-2.0/mod_jk.c
+++ jk/native/apache-2.0/mod_jk.c
@@ -2354,6 +2354,9 @@ static void *create_jk_config(apr_pool_t * p, server_rec * s)
         c->ssl_enable = JK_UNSET;
         c->strip_session = JK_UNSET;
     } else {
+        if (!jk_map_alloc(&(c->uri_to_context))) {
+            ap_log_error(APLOG_MARK, APLOG_ERR, 0, NULL, "Memory error");
+        }
         c->mountcopy = JK_FALSE;
         c->mount_file_reload = JK_URIMAP_DEF_RELOAD;
         c->log_level = JK_LOG_DEF_LEVEL;
