dda6fef6ac60d3e7f9c93d6e9d5e47e694ab7909
Mladen Turk <mturk@apache.org>
2008-10-19 16:06:35 +0000

    Use optional APR pool used for configuration instead global pool.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@706041 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native/common/jk_ajp12_worker.c
+++ jk/native/common/jk_ajp12_worker.c
@@ -192,7 +192,7 @@ static int JK_METHOD validate(jk_worker_t *pThis,
                p->name, host, port);
 
         if (port > 1024 && host) {
-            if (jk_resolve(host, port, &p->worker_inet_addr, l)) {
+            if (jk_resolve(host, port, &p->worker_inet_addr, we->pool, l)) {
                 return JK_TRUE;
             }
             jk_log(l, JK_LOG_ERROR,
