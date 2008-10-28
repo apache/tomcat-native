8401a9cba9977486b3ad19f92d69307a6b30bc72
Rainer Jung <rjung@apache.org>
2008-10-28 21:48:05 +0000

    Update STATUS and version file to prepare tagging 1.2.27.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk@708688 13f79535-47bb-0310-9956-ffa450edef68

--- jk/native/common/jk_version.h
+++ jk/native/common/jk_version.h
@@ -30,7 +30,7 @@
 #define JK_VERSTRING    "1.2.27"
 
 /* set JK_VERISRELEASE to 1 when release (do not forget to commit!) */
-#define JK_VERISRELEASE 0
+#define JK_VERISRELEASE 1
 /* Beta number */
 #define JK_VERBETA      0
 #define JK_BETASTRING   "0"
