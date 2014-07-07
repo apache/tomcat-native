5122d00ae3c3aa266bda55483644c9976d250ab3
Mark Emlyn David Thomas <markt@apache.org>
2014-07-07 11:41:34 +0000

    Stop OSX storing extended attributes in additional files in tar files
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/native/trunk@1608416 13f79535-47bb-0310-9956-ffa450edef68

--- jnirelease.sh
+++ jnirelease.sh
@@ -22,6 +22,10 @@ JKJNIEXT=""
 JKJNIVER=""
 SVNBASE=https://svn.apache.org/repos/asf/tomcat/native
 
+# Set the environment variable that stops OSX including storing extended
+# attributes in tar archives etc with a file starting with ._
+export COPYFILE_DISABLE=1
+
 for o
 do
     case "$o" in
