332a8a59f585fb3d37389b963fe9fbbdd93d30cb
Rainer Jung <rjung@apache.org>
2015-10-31 10:04:26 +0000

    Fix compiler warning: remove unused variable.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/native/trunk@1711583 13f79535-47bb-0310-9956-ffa450edef68

--- native/src/ssl.c
+++ native/src/ssl.c
@@ -641,7 +641,6 @@ static int ssl_rand_make(const char *file, int len, int base64)
 
 TCN_IMPLEMENT_CALL(jint, SSL, initialize)(TCN_STDARGS, jstring engine)
 {
-    int r = 0;
     jclass clazz;
     jclass sClazz;
 
