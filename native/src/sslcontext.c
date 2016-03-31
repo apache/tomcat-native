0760da3199eca5bd3ad02675d5734dbecee7e684
Mark Thomas <markt@apache.org>
2016-03-31 07:13:38 +0000

    Add missing stub methods for when OpenSSL is not available Review from kkolinko
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/native/trunk@1737183 13f79535-47bb-0310-9956-ffa450edef68

--- native/src/sslcontext.c
+++ native/src/sslcontext.c
@@ -2018,6 +2018,27 @@ TCN_IMPLEMENT_CALL(jboolean, SSLContext, setCertificate)(TCN_STDARGS, jlong ctx,
     return JNI_FALSE;
 }
 
+TCN_IMPLEMENT_CALL(jboolean, SSLContext, setCertificateRaw)(TCN_STDARGS, jlong ctx,
+                                                         jbyteArray javaCert, jbyteArray javaKey,
+                                                         jint idx)
+{
+    UNREFERENCED_STDARGS;
+    UNREFERENCED(ctx);
+    UNREFERENCED(javaCert);
+    UNREFERENCED(javaKey);
+    UNREFERENCED(idx);
+    return JNI_FALSE;
+}
+
+TCN_IMPLEMENT_CALL(jboolean, SSLContext, addChainCertificateRaw)(TCN_STDARGS, jlong ctx,
+                                                                 jbyteArray javaCert)
+{
+    UNREFERENCED_STDARGS;
+    UNREFERENCED(ctx);
+    UNREFERENCED(javaCert);
+    return JNI_FALSE;
+}
+
 TCN_IMPLEMENT_CALL(jint, SSLContext, setALPN)(TCN_STDARGS, jlong ctx,
                                               jbyteArray buf, jint len)
 {
