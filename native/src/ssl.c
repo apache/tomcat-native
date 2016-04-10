eea4483549a712c56eb74e86e51429605690dc46
Rainer Jung <rjung@apache.org>
2016-04-10 09:18:40 +0000

    Followup to r1738408: - stay compatible with OpenSSL 1.0.2.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/native/trunk@1738411 13f79535-47bb-0310-9956-ffa450edef68

--- native/src/ssl.c
+++ native/src/ssl.c
@@ -193,6 +193,29 @@ static const jint supported_ssl_opts = 0
 #endif
      | 0;
 
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+/* OpenSSL Pre-1.1.0 compatibility */
+/* Taken from OpenSSL 1.1.0 snapshot 20160410 */
+int DH_set0_pqg(DH *dh, BIGNUM *p, BIGNUM *q, BIGNUM *g)
+{
+    /* q is optional */
+    if (p == NULL || g == NULL)
+        return 0;
+    BN_free(dh->p);
+    BN_free(dh->q);
+    BN_free(dh->g);
+    dh->p = p;
+    dh->q = q;
+    dh->g = g;
+
+    if (q != NULL) {
+        dh->length = BN_num_bits(q);
+    }
+
+    return 1;
+}
+#endif
+
 /*
  * Grab well-defined DH parameters from OpenSSL, see the get_rfc*
  * functions in <openssl/bn.h> for all available primes.
