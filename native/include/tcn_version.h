1259cd9874b722aeb6382cd2dc9c531a58312926
Mark Thomas <markt@apache.org>
2016-09-26 15:48:44 +0000

    Prep for next tag
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/native/trunk@1762347 13f79535-47bb-0310-9956-ffa450edef68

--- native/include/tcn_version.h
+++ native/include/tcn_version.h
@@ -63,7 +63,7 @@ extern "C" {
 #define TCN_MINOR_VERSION       2
 
 /** patch level */
-#define TCN_PATCH_VERSION       10
+#define TCN_PATCH_VERSION       11
 
 /**
  *  This symbol is defined for internal, "development" copies of TCN. This
