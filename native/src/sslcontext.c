d6a9033ea5c5e0473a2716da5f0af66946574a19
Mark Thomas <markt@apache.org>
2017-08-23 11:43:30 +0000

    Fix a warning when compiling with LibreSSL
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/native/trunk@1805883 13f79535-47bb-0310-9956-ffa450edef68

--- native/src/sslcontext.c
+++ native/src/sslcontext.c
@@ -1211,9 +1211,14 @@ TCN_IMPLEMENT_CALL(jboolean, SSLContext, addChainCertificateRaw)(TCN_STDARGS, jl
         ERR_error_string(SSL_ERR_get(), err);
         tcn_Throw(e, "Error reading certificate (%s)", err);
         rv = JNI_FALSE;
+#if defined(LIBRESSL_VERSION_NUMBER)
+    } else {
+        tcn_Throw(e, "Unable to use Java keystores with LibreSSL");
+#else
     } else if (SSL_CTX_add0_chain_cert(c->ctx, certs) <= 0) {
         ERR_error_string(SSL_ERR_get(), err);
         tcn_Throw(e, "Error adding certificate to chain (%s)", err);
+#endif
         rv = JNI_FALSE;
     }
 
