6bb8832698ad3754c1f395366f10cb78161d6093
Jean-Frederic Clere <jfclere@gmail.com>
2018-09-04 16:04:03 +0200

    Add the logic to build on macosx.

--- copylibs_deps.sh
+++ copylibs_deps.sh
@@ -1,21 +1,36 @@
 #!/bin/sh
 
 # Small script to copy the libraries needed by tc-native.
+TC_OS=`uname -s`
+LIB_SUFIX=so
+LDD_TOOL=ldd
+case $TC_OS in
+  Darwin)
+    echo "Using Darwin!!!"
+    LIB_SUFIX=dylib
+    LDD_TOOL="otool -L"
+    ;;
+  CYGWIN*)
+    echo "Using CYGWIN!!!"
+    LIB_SUFIX=dll
+    ;;
+esac
+
 TARGET=target/classes/`uname -s`-`uname -p`/lib
+rm -rf $TARGET
 mkdir -p $TARGET
 
-echo "The libraries with be copied to $TARGET"
+echo "The libraries will be copied to $TARGET"
 
-TC_LIB=`find . -name *.so | head -1`
+TC_LIB=`find . -name *.$LIB_SUFIX | head -1`
 
 cp $TC_LIB $TARGET
 
 NAMETC_LIB=`basename $TC_LIB`
 
-for file in `ldd $TARGET/$NAMETC_LIB`
+for file in `$LDD_TOOL $TARGET/$NAMETC_LIB`
 do
   lib=`echo $file | awk '$1 ~ /^\// '`
-  echo $lib
   case $lib in
     *ssl*)
       cp $lib $TARGET
@@ -23,7 +38,7 @@ do
     *crypto*)
       cp $lib $TARGET
       ;;
-    *apr*)
+    *libapr*)
       cp $lib $TARGET
       ;;
     esac
