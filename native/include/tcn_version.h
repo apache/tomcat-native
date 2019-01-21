b14d779d0d023f2a6a6857153057f049bf61a65d
Mark Thomas <markt@apache.org>
2019-01-21 14:09:47 +0000

    Increment version info ready for next development cycle
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/native/trunk@1851743 13f79535-47bb-0310-9956-ffa450edef68

--- native/include/tcn_version.h
+++ native/include/tcn_version.h
@@ -63,7 +63,7 @@ extern "C" {
 #define TCN_MINOR_VERSION       2
 
 /** patch level */
-#define TCN_PATCH_VERSION       20
+#define TCN_PATCH_VERSION       21
 
 /**
  *  This symbol is defined for internal, "development" copies of TCN. This
