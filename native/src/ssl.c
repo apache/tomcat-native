2d3257974fc5eef51a0c6d5946b28e42f84bc180
Michael Osipov <michaelo@apache.org>
2020-03-09 14:55:38 +0100

    Add support for FreeBSD's pthread_getthreadid_np()
    Call directly pthread_getthreadid_np() in ssl_thread_id(void).
    
    Patch based on the FreeBSD Port of tomcat-native:
    https://svnweb.freebsd.org/ports/head/www/tomcat-native/files/patch-src_ssl.c?revision=528004&view=o

--- native/src/ssl.c
+++ native/src/ssl.c
@@ -24,6 +24,10 @@
 #include <sys/syscall.h>
 #endif
 
+#ifdef __FreeBSD__
+#include <pthread_np.h>
+#endif
+
 #ifdef HAVE_OPENSSL
 #include "ssl_private.h"
 
@@ -478,6 +482,8 @@ static unsigned long ssl_thread_id(void)
     uint64_t tid;
     pthread_threadid_np(NULL, &tid);
     return (unsigned long)tid;
+#elif defined(__FreeBSD__)
+    return (unsigned long)pthread_getthreadid_np();
 #elif defined(__linux__)
     return (unsigned long)syscall(SYS_gettid);
 #else
