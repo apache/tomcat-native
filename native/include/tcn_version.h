ebf0f74e35a6a27ffc68765a572306f53d8410c0
Michael Osipov <michaelo@apache.org>
2020-05-11 15:14:01 +0200

    Bump version to 1.2.25-dev

--- native/include/tcn_version.h
+++ native/include/tcn_version.h
@@ -63,7 +63,7 @@ extern "C" {
 #define TCN_MINOR_VERSION       2
 
 /** patch level */
-#define TCN_PATCH_VERSION       24
+#define TCN_PATCH_VERSION       25
 
 /**
  *  This symbol is defined for internal, "development" copies of TCN. This
