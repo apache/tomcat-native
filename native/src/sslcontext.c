793c5bfe79f44be037bfa11ad6817b70b983b4a9
gastush <gastush@gmail.com>
2025-09-26 20:18:35 +0200

    Use the Global delete function for Global references

--- native/src/sslcontext.c
+++ native/src/sslcontext.c
@@ -1860,7 +1860,7 @@ TCN_IMPLEMENT_CALL(void, SSLContext, setCertVerifyCallback)(TCN_STDARGS, jlong c
         }
         // Delete the reference to the previous specified verifier if needed.
         if (c->verifier != NULL) {
-            (*e)->DeleteLocalRef(e, c->verifier);
+            (*e)->DeleteGlobalRef(e, c->verifier);
         }
         c->verifier = (*e)->NewGlobalRef(e, verifier);
         c->verifier_method = method;
