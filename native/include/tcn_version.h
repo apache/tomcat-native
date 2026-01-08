60e9b76ed569397c63922ca207fbeb1a92f69e56
Mark Thomas <markt@apache.org>
2026-01-08 14:51:07 +0000

    Increment version for 1.3.5 development

--- native/include/tcn_version.h
+++ native/include/tcn_version.h
@@ -63,7 +63,7 @@ extern "C" {
 #define TCN_MINOR_VERSION       3
 
 /** patch level */
-#define TCN_PATCH_VERSION       4
+#define TCN_PATCH_VERSION       5
 
 /**
  *  This symbol is defined for internal, "development" copies of TCN. This
