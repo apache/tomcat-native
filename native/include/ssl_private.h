a062ba8f04912bdb129525095dc9b560e6c9fec7
Mark Thomas <markt@apache.org>
2026-08-27 10:03:13 +0100

    Remove unused code

--- native/include/ssl_private.h
+++ native/include/ssl_private.h
@@ -283,7 +283,6 @@ struct tcn_ssl_ctxt_t {
     int             shutdown_type;
     char            *rand_file;
 
-    const char      *cipher_suite;
     /* for client or downstream server authentication */
     int             verify_depth;
     int             verify_mode;
