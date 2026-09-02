c5a376c93da7b05837dccca239251f1d34f1286b
Mark Thomas <markt@apache.org>
2026-09-02 08:11:16 +0100

    Align Java code with lastest Tomcat code

--- java/org/apache/tomcat/jni/Pool.java
+++ java/org/apache/tomcat/jni/Pool.java
@@ -17,8 +17,8 @@
 package org.apache.tomcat.jni;
 
 /**
-  * Provides access to APR memory pools which are used to manage memory allocations for natively created instances.
-  */
+ * Provides access to APR memory pools which are used to manage memory allocations for natively created instances.
+ */
 public class Pool {
 
     /**
@@ -39,7 +39,7 @@ public class Pool {
 
     /**
      * Destroy the pool. This takes similar action as apr_pool_clear() and then frees all the memory. This will actually
-     * free the memory.
+     * free the memory. Calling this more than once for a pool will trigger a JVM crash.
      *
      * @param pool The pool to destroy
      */
